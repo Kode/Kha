138e9a20277efacf17dd6d9859da582f2e89abab
Robert Konrad <robert@kontechs.de>
2011-05-01 02:21:44 +0200

    GWT backend working again.

--- backends/gwt/com/kontechs/kje/backends/gwt/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package com.kontechs.kje.backends.gwt;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-@RemoteServiceRelativePath("greet")
-public interface GreetingService extends RemoteService {
-	int[][] getLevel();
-}
--- backends/gwt/com/kontechs/kje/backends/gwt/GreetingServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.kontechs.kje.backends.gwt;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface GreetingServiceAsync {
-	void getLevel(AsyncCallback<int[][]> callback);
-}
