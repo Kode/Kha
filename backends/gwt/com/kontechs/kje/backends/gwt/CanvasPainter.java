130ad804ecabb9ab8a3ff9968f109b4542f4b147
Robert Konrad <robert@kontechs.de>
2011-07-16 04:29:56 +0200

    Started implementing WebGL backend.

--- backends/gwt/com/kontechs/kje/backends/gwt/CanvasPainter.java
+++ backends/gwt/com/kontechs/kje/backends/gwt/CanvasPainter.java
@@ -1,44 +1,153 @@
 package com.kontechs.kje.backends.gwt;
 
+import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.canvas.dom.client.CssColor;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.ui.FocusPanel;
+import com.googlecode.gwtgl.array.Float32Array;
+import com.googlecode.gwtgl.array.TypedArray;
+import com.googlecode.gwtgl.binding.WebGLBuffer;
+import com.googlecode.gwtgl.binding.WebGLCanvas;
+import com.googlecode.gwtgl.binding.WebGLProgram;
+import com.googlecode.gwtgl.binding.WebGLRenderingContext;
+import com.googlecode.gwtgl.binding.WebGLShader;
+import com.googlecode.gwtgl.binding.WebGLUniformLocation;
 
 import com.kontechs.kje.Font;
 import com.kontechs.kje.Image;
 import com.kontechs.kje.Painter;
 
 public class CanvasPainter implements Painter {
+	private WebGLRenderingContext glContext;
+    private WebGLProgram shaderProgram;
+    private int vertexPositionAttribute;
+    private WebGLBuffer vertexBuffer;
+
+    private Canvas canvas;
 	private Context2d context;
 	private double tx, ty;
 	
-	public CanvasPainter(Context2d context) {
-		this.context = context;
+	public CanvasPainter(FocusPanel panel, int width, int height) {
+		final WebGLCanvas webGLCanvas = new WebGLCanvas(width + "px", height + "px");
+        glContext = webGLCanvas.getGlContext();
+        glContext.viewport(0, 0, width, height);
+        panel.add(webGLCanvas);
+		
+        initShaders();
+        glContext.clearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glContext.clearDepth(1.0f);
+        glContext.enable(WebGLRenderingContext.DEPTH_TEST);
+        glContext.depthFunc(WebGLRenderingContext.LEQUAL);
+        initBuffers();
+        
+        drawScene();
+        
+		/*canvas = Canvas.createIfSupported();
+		canvas.setWidth(width + "px");
+		canvas.setHeight(height + "px");
+		canvas.setCoordinateSpaceWidth(width);
+		canvas.setCoordinateSpaceHeight(height);
+		context = canvas.getContext2d();
+		
+		panel.add(canvas);*/
+	}
+	
+	private void initShaders() {
+        WebGLShader fragmentShader = getShader(WebGLRenderingContext.FRAGMENT_SHADER, "void main(void)\n{\ngl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n}");
+        WebGLShader vertexShader = getShader(WebGLRenderingContext.VERTEX_SHADER, "attribute vec3 vertexPosition;\nuniform mat4 perspectiveMatrix;\nvoid main(void)\n{\ngl_Position = perspectiveMatrix * vec4(vertexPosition, 1.0);\n}");
+
+        shaderProgram = glContext.createProgram();
+        glContext.attachShader(shaderProgram, vertexShader);
+        glContext.attachShader(shaderProgram, fragmentShader);
+        glContext.linkProgram(shaderProgram);
+
+        if (!glContext.getProgramParameterb(shaderProgram, WebGLRenderingContext.LINK_STATUS)) {
+                throw new RuntimeException("Could not initialise shaders");
+        }
+
+        glContext.useProgram(shaderProgram);
+
+        vertexPositionAttribute = glContext.getAttribLocation(shaderProgram, "vertexPosition");
+        glContext.enableVertexAttribArray(vertexPositionAttribute);
+	}
+	
+	private WebGLShader getShader(int type, String source) {
+        WebGLShader shader = glContext.createShader(type);
+
+        glContext.shaderSource(shader, source);
+        glContext.compileShader(shader);
+
+        if (!glContext.getShaderParameterb(shader, WebGLRenderingContext.COMPILE_STATUS)) {
+                throw new RuntimeException(glContext.getShaderInfoLog(shader));
+        }
+
+        return shader;
+	}
+	
+	private void initBuffers() {
+		vertexBuffer = glContext.createBuffer();
+		glContext.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, vertexBuffer);
+		float[] vertices = new float[]{
+				0.0f,  1.0f,  -5.0f, // first vertex
+				-1.0f, -1.0f,  -5.0f, // second vertex
+				1.0f, -1.0f,  -5.0f  // third vertex
+		};
+		glContext.bufferData(WebGLRenderingContext.ARRAY_BUFFER, Float32Array.create(vertices), WebGLRenderingContext.STATIC_DRAW);
+	}
+	
+	private float[] createPerspectiveMatrix(int fieldOfViewVertical, float aspectRatio, float minimumClearance, float maximumClearance) {
+        float top    = minimumClearance * (float)Math.tan(fieldOfViewVertical * Math.PI / 360.0);
+        float bottom = -top;
+        float left   = bottom * aspectRatio;
+        float right  = top * aspectRatio;
+
+        float X = 2*minimumClearance/(right-left);
+        float Y = 2*minimumClearance/(top-bottom);
+        float A = (right+left)/(right-left);
+        float B = (top+bottom)/(top-bottom);
+        float C = -(maximumClearance+minimumClearance)/(maximumClearance-minimumClearance);
+        float D = -2*maximumClearance*minimumClearance/(maximumClearance-minimumClearance);
+
+        return new float[]{     X, 0.0f, A, 0.0f,
+                                                0.0f, Y, B, 0.0f,
+                                                0.0f, 0.0f, C, -1.0f,
+                                                0.0f, 0.0f, D, 0.0f};
+	}
+	
+	void drawScene() {
+		glContext.clear(WebGLRenderingContext.COLOR_BUFFER_BIT | WebGLRenderingContext.DEPTH_BUFFER_BIT);
+        float[] perspectiveMatrix = createPerspectiveMatrix(45, 1, 0.1f, 1000);
+        WebGLUniformLocation uniformLocation = glContext.getUniformLocation(shaderProgram, "perspectiveMatrix");
+        glContext.uniformMatrix4fv(uniformLocation, false, perspectiveMatrix);
+        glContext.vertexAttribPointer(vertexPositionAttribute, 3, WebGLRenderingContext.FLOAT, false, 0, 0);
+        glContext.drawArrays(WebGLRenderingContext.TRIANGLES, 0, 3);
 	}
 	
 	@Override
 	public void drawImage(Image img, double x, double y) {
-		context.drawImage(((WebImage)img).getIE(), tx + x, ty + y);
+		//context.drawImage(((WebImage)img).getIE(), tx + x, ty + y);
 	}
 	
 	@Override
 	public void drawImage(Image img, double sx, double sy, double sw, double sh, double dx, double dy, double dw, double dh) {
-		context.drawImage(((WebImage)img).getIE(), sx, sy, sw, sh, tx + dx, ty + dy, dw, dh);
+		//context.drawImage(((WebImage)img).getIE(), sx, sy, sw, sh, tx + dx, ty + dy, dw, dh);
 	}
 	
 	@Override
 	public void setColor(int r, int g, int b) {
-		context.setStrokeStyle(CssColor.make(r, g, b));
-		context.setFillStyle(CssColor.make(r, g, b));
+		//context.setStrokeStyle(CssColor.make(r, g, b));
+		//context.setFillStyle(CssColor.make(r, g, b));
 	}
 	
 	@Override
 	public void drawRect(double x, double y, double width, double height) {
-		context.rect(tx + x, ty + y, width, height);
+		//context.rect(tx + x, ty + y, width, height);
 	}
 	
 	@Override
 	public void fillRect(double x, double y, double width, double height) {
-		context.fillRect(tx + x, ty + y, width, height);
+		//context.fillRect(tx + x, ty + y, width, height);
 	}
 
 	@Override
@@ -49,12 +158,12 @@ public class CanvasPainter implements Painter {
 
 	@Override
 	public void drawString(String text, double x, double y) {
-		context.fillText(text, tx + x, ty + y);
+		//context.fillText(text, tx + x, ty + y);
 	}
 
 	@Override
 	public void setFont(Font font) {
-		context.setFont(((WebFont)font).name);
+		//context.setFont(((WebFont)font).name);
 	}
 
 	@Override
@@ -64,16 +173,16 @@ public class CanvasPainter implements Painter {
 
 	@Override
 	public void drawLine(double x1, double y1, double x2, double y2) {
-		context.moveTo(tx + x1, ty + y1);
+		/*context.moveTo(tx + x1, ty + y1);
 		context.lineTo(tx + x2, ty + y2);
-		context.moveTo(0, 0);
+		context.moveTo(0, 0);*/
 	}
 
 	@Override
 	public void fillTriangle(double x1, double y1, double x2, double y2, double x3, double y3) {
-		context.beginPath();
+		/*context.beginPath();
 		
 		context.closePath();
-		context.fill();
+		context.fill();*/
 	}
 }
--- backends/gwt/com/kontechs/kje/backends/gwt/Game_gwt.java
+++ backends/gwt/com/kontechs/kje/backends/gwt/Game_gwt.java
@@ -1,7 +1,5 @@
 package com.kontechs.kje.backends.gwt;
 
-import com.google.gwt.canvas.client.Canvas;
-import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
@@ -13,7 +11,6 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.RootPanel;
-
 import com.kontechs.kje.Game;
 import com.kontechs.kje.GameInfo;
 import com.kontechs.kje.Key;
@@ -34,8 +31,6 @@ public class Game_gwt implements EntryPoint {
 }
 
 class AnimationTimer extends Timer implements KeyDownHandler, KeyUpHandler, KeyPressHandler {
-	private Canvas canvas;
-	private Context2d context;
 	private CanvasPainter painter;
 	private static final int WIDTH = 640;
 	private static final int HEIGHT = 550;
@@ -46,17 +41,9 @@ class AnimationTimer extends Timer implements KeyDownHandler, KeyUpHandler, KeyP
 			keyreleased = new boolean[256];
 			for (int i = 0; i < 256; ++i) keyreleased[i] = true;
 			
-			canvas = Canvas.createIfSupported();
-			canvas.setWidth(WIDTH + "px");
-			canvas.setHeight(HEIGHT + "px");
-			canvas.setCoordinateSpaceWidth(WIDTH);
-			canvas.setCoordinateSpaceHeight(HEIGHT);
-			context = canvas.getContext2d();
-			painter = new CanvasPainter(context);
-	
 			FocusPanel panel = new FocusPanel(); //Canvas can not receive key events in IE9
 			RootPanel.get().add(panel);
-			panel.add(canvas);
+			painter = new CanvasPainter(panel, WIDTH, HEIGHT);
 			panel.addKeyDownHandler(this);
 			panel.addKeyUpHandler(this);
 			panel.addKeyPressHandler(this);
