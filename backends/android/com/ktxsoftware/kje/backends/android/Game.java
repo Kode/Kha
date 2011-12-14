c3f2aa9148393a7d5fc2b364cf1372f36c7a4242
Robert Konrad <robert@ktx-software.com>
2011-12-14 20:58:44 +0100

    Reverted Android to canvas graphics backend.

--- backends/android/com/ktxsoftware/kje/backends/android/Game.java
+++ backends/android/com/ktxsoftware/kje/backends/android/Game.java
@@ -3,35 +3,24 @@ package com.ktxsoftware.kje.backends.android;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.LinearLayout;
 
 public class Game extends Activity {
+	private LinearLayout layout;
 
-    private GameView mView;
-
-    @Override
-    protected void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        mView = new GameView(getApplication());
-        setContentView(mView);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mView.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mView.onResume();
-    }
-    
-    /*@Override
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		layout = new LinearLayout(this);
+		layout.addView(new GameView(this));
+		setContentView(layout);
+	}
+	
+	@Override
 	public void onStop() {
 		super.onStop();
 		AndroidMusic.stopit();
 		finish();
-	}*/
+	}
 }
--- backends/android/com/ktxsoftware/kje/backends/android/GameView.java
+++ backends/android/com/ktxsoftware/kje/backends/android/GameView.java
@@ -1,45 +1,105 @@
 package com.ktxsoftware.kje.backends.android;
 
 import android.content.Context;
-import android.opengl.GLSurfaceView;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class GameView extends GLSurfaceView {
-	private OpenGLES20Renderer renderer;
-	
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+	private GameThread thread;
+	//private int lastTouch;
+
 	public GameView(Context context) {
 		super(context);
-		setEGLContextClientVersion(2);
-		renderer = new OpenGLES20Renderer(context);
-        setRenderer(renderer);
-        setFocusable(true);
-		requestFocus();
+		SurfaceHolder holder = getHolder();
+		holder.addCallback(this);
+		thread = new GameThread(holder, context, getWidth(), getHeight());
+		setFocusable(true);
 	}
 
 	public boolean onTouchEvent(MotionEvent event) {
+		//if (gestureDetector == null) setupGestureDetector();
+		//gestureDetector.onTouchEvent(event);
+
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-			renderer.mouseDown((int)event.getX(), (int)event.getY());
+			thread.mouseDown((int)event.getX(), (int)event.getY());
 			break;
 		case MotionEvent.ACTION_MOVE:
-			renderer.mouseMove((int)event.getX(), (int)event.getY());
+			thread.mouseMove((int)event.getX(), (int)event.getY());
 			break;
 		case MotionEvent.ACTION_UP:
-			renderer.mouseUp((int)event.getX(), (int)event.getY());
+			thread.mouseUp((int)event.getX(), (int)event.getY());
 			break;
 		}
+		
+		/*if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			if (event.getRawY() < getHeight() / 2) {
+				lastTouch = 0;
+				thread.keyDown(KeyEvent.KEYCODE_DPAD_UP);
+			}
+			else if (event.getRawX() < getWidth() / 2) {
+				lastTouch = 1;
+				thread.keyDown(KeyEvent.KEYCODE_DPAD_LEFT);
+			}
+			else {
+				lastTouch = 2;
+				thread.keyDown(KeyEvent.KEYCODE_DPAD_RIGHT);
+			}
+		}
+		else if (event.getAction() == MotionEvent.ACTION_UP) {
+			switch (lastTouch) {
+			case 0:
+				thread.keyUp(KeyEvent.KEYCODE_DPAD_UP);
+				break;
+			case 1:
+				thread.keyUp(KeyEvent.KEYCODE_DPAD_LEFT);
+				break;
+			case 2:
+				thread.keyUp(KeyEvent.KEYCODE_DPAD_RIGHT);
+				break;
+			}
+		}*/
 		return true;
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent msg) {
 		if (keyCode == KeyEvent.KEYCODE_BACK && msg.isAltPressed()) return true; //Circle button on Xperia Play
-		return renderer.keyDown(keyCode);
+		return thread.keyDown(keyCode);
 	}
 
 	@Override
 	public boolean onKeyUp(int keyCode, KeyEvent msg) {
-		return renderer.keyUp(keyCode);
+		return thread.keyUp(keyCode);
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasWindowFocus) {
+
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		thread.setSurfaceSize(width, height);
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		thread.setRunning(true);
+		thread.start();
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		boolean retry = true;
+		thread.setRunning(false);
+		while (retry) {
+			try {
+				thread.join();
+				retry = false;
+			}
+			catch (InterruptedException e) {
+
+			}
+		}
 	}
 }
