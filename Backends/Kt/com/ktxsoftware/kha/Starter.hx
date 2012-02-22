9856df8d6d75369768c617644a99a7a68f090e0e
Robert Konrad <robert@ktx-software.com>
2012-02-22 20:42:45 +0100

    Added Button1 support to Kt backend.

--- Backends/Kt/com/ktxsoftware/kha/Starter.hx
+++ Backends/Kt/com/ktxsoftware/kha/Starter.hx
@@ -4,154 +4,6 @@ import com.ktxsoftware.kha.Game;
 import com.ktxsoftware.kha.Key;
 import com.ktxsoftware.kha.Loader;
 
-#if flash
-
-import com.ktxsoftware.kha.backends.flash.Painter;
-import flash.events.Event;
-import flash.events.KeyboardEvent;
-import flash.events.MouseEvent;
-import flash.Lib;
-import flash.display.MovieClip;
-import flash.display.Sprite;
-
-class Starter extends MovieClip {
-	var game : Game;
-	var painter : Painter;
-	
-	public function new() {
-		super();
-		Loader.init(new com.ktxsoftware.kha.backends.flash.Loader(this));
-	}
-	
-	public function start(game : Game) {
-		this.game = game;
-		Loader.getInstance().load();
-	}
-	
-	public function loadFinished() {
-		game.init();
-		painter = new Painter();
-		Lib.current.addChild(this);
-		stage.frameRate = 60;
-		Lib.current.addEventListener(Event.ENTER_FRAME, draw);
-		stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDownHandler);
-		stage.addEventListener(KeyboardEvent.KEY_UP, keyUpHandler);
-	}
-
-	function draw(e : Event) {
-		//game.update();
-		game.update();
-		painter.setGraphics(graphics);
-		painter.begin();
-		game.render(painter);
-		painter.end();
-	}
-	
-	function keyDownHandler(event : KeyboardEvent) {
-		switch (event.keyCode) {
-		case 38:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.UP, true));
-		case 40:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.DOWN, true));
-		case 37:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.LEFT, true));
-		case 39:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.RIGHT, true));
-		}
-	}
-
-	function keyUpHandler(event : KeyboardEvent) {
-		switch (event.keyCode) {
-		case 38:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.UP, false));
-		case 40:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.DOWN, false));
-		case 37:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.LEFT, false));
-		case 39:
-			game.key(new com.ktxsoftware.kha.KeyEvent(Key.RIGHT, false));
-		}
-	}
-}
-
-#end
-
-#if js
-
-import js.Lib;
-import js.Dom;
-
-class Starter {
-	static var game : Game;
-	static var painter : com.ktxsoftware.kha.backends.js.Painter;
-	
-	public function new() {
-		Loader.init(new com.ktxsoftware.kha.backends.js.Loader());
-	}
-	
-	public function start(game : Game) : Void {
-		Starter.game = game;
-		Loader.getInstance().load();
-	}
-	
-	public static function loadFinished() {
-		game.init();
-		painter = new com.ktxsoftware.kha.backends.js.Painter();
-
-		Lib.document.onkeydown = function(event : js.Event) {
-			switch (event.keyCode) {
-			case 38:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.UP, true));
-			case 40:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.DOWN, true));
-			case 37:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.LEFT, true));
-			case 39:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.RIGHT, true));
-			}
-		};
-		Lib.document.onkeyup = function(event : js.Event) {
-			switch (event.keyCode) {
-			case 38:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.UP, false));
-			case 40:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.DOWN, false));
-			case 37:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.LEFT, false));
-			case 39:
-				game.key(new com.ktxsoftware.kha.KeyEvent(Key.RIGHT, false));
-			}
-		};
-		var window : Dynamic = Lib.window;
-		var requestAnimationFrame = window.requestAnimationFrame;
-		if (requestAnimationFrame == null) requestAnimationFrame = window.mozRequestAnimationFrame;
-		if (requestAnimationFrame == null) requestAnimationFrame = window.webkitRequestAnimationFrame;
-		if (requestAnimationFrame == null) requestAnimationFrame = window.msRequestAnimationFrame;
-		
-		function animate(timestamp) {
-			var window : Dynamic = Lib.window;
-			if (requestAnimationFrame == null) window.setTimeout(animate, 1000.0 / 60.0);
-			else requestAnimationFrame(animate);			
-			game.update();
-			var canvas : Dynamic = Lib.document.getElementById("haxvas");
-			if (canvas.getContext) {
-				canvas = canvas.getContext('2d');
-				painter.setCanvas(canvas);
-				painter.begin();
-				game.render(painter);
-				painter.end();
-			}
-		}
-		
-		if (requestAnimationFrame == null) window.setTimeout(animate, 1000.0 / 60.0);
-		else requestAnimationFrame(animate);
-	}
-}
-
-#end
-
-#if cpp
-
 class Starter {
 	static var game : Game;
 	static var painter : com.ktxsoftware.kha.backends.cpp.Painter;
@@ -192,6 +44,10 @@ class Starter {
 	public static function pushRight() : Void {
 		game.key(new com.ktxsoftware.kha.KeyEvent(Key.RIGHT, true));
 	}
+	
+	public static function pushButton1() : Void {
+		game.key(new com.ktxsoftware.kha.KeyEvent(Key.BUTTON_1, true));
+	}
 
 	public static function releaseUp() : Void {
 		game.key(new com.ktxsoftware.kha.KeyEvent(Key.UP, false));
@@ -208,6 +64,8 @@ class Starter {
 	public static function releaseRight() : Void {
 		game.key(new com.ktxsoftware.kha.KeyEvent(Key.RIGHT, false));
 	}
+	
+	public static function releaseButton1() : Void {
+		game.key(new com.ktxsoftware.kha.KeyEvent(Key.BUTTON_1, false));
+	}
 }
-
-#end
