811c6ded77d68a299d969cd6ebefe6d9e858d587
Robert Konrad <robert@ktx-software.com>
2012-04-15 11:19:31 +0200

    Added missing methods in canvas painter. Added CSSPainter.

---
+++ Backends/HTML5/com/ktxsoftware/kha/backends/js/Font.hx
@@ -0,0 +1,37 @@
+package com.ktxsoftware.kha.backends.js;
+
+import com.ktxsoftware.kha.FontStyle;
+
+class Font implements com.ktxsoftware.kha.Font {
+	public var name : String;
+	public var style : FontStyle;
+	public var size : Int;
+	
+	public function new(name : String, style : FontStyle, size : Int) {
+		this.name = name;
+		this.style = style;
+		this.size = size;
+	}
+	
+	public function getHeight() : Float {
+		return size;
+	}
+
+	public function charWidth(ch : String) : Float {
+		return stringWidth(ch);
+	}
+
+	public function charsWidth(ch : String, offset : Int, length : Int) : Float {
+		return stringWidth(ch.substr(offset, length));
+	}
+
+	public function stringWidth(str : String) : Float {
+		//CanvasPainter.getInstance().setFont(this);
+		//return CanvasPainter.getInstance().context.measureText(str).getWidth();
+		return 5 * str.length;
+	}
+
+	public function getBaselinePosition() : Float {
+		return 0;
+	}
+}
--- Backends/HTML5/com/ktxsoftware/kha/backends/js/Painter.hx
+++ Backends/HTML5/com/ktxsoftware/kha/backends/js/Painter.hx
@@ -15,20 +15,64 @@ class Painter extends com.ktxsoftware.kha.Painter {
 		ty = 0;
 	}
 	
-	public override function begin() {
+	override public function begin() {
 		canvas.clearRect(0, 0, width, height);
 	}
 	
-	public override function end() {
+	override public function end() {
 		
 	}
 	
-	public override function translate(x : Float, y : Float) {
+	override public function translate(x : Float, y : Float) {
 		tx = x;
 		ty = y;
 	}
 	
-	public override function drawImage2(image : com.ktxsoftware.kha.Image, sx : Float, sy : Float, sw : Float, sh : Float, dx : Float, dy : Float, dw : Float, dh : Float) {
-		canvas.drawImage(cast(image, Image).image, sx, sy, sw, sh, tx + dx, ty + dy, dw, dh);
+	override public function drawImage(img : com.ktxsoftware.kha.Image, x : Float, y : Float) {
+		canvas.drawImage(cast(img, Image).image, tx + x, ty + y);
+	}
+	
+	override public function drawImage2(image : com.ktxsoftware.kha.Image, sx : Float, sy : Float, sw : Float, sh : Float, dx : Float, dy : Float, dw : Float, dh : Float) {
+		try {
+			canvas.drawImage(cast(image, Image).image, sx, sy, sw, sh, tx + dx, ty + dy, dw, dh);
+		}
+		catch (ex : Dynamic) {
+			
+		}
+	}
+	
+	override public function setColor(r : Int, g : Int, b : Int) {
+		canvas.strokeStyle = "rgb(" + r + "," + g + "," + b + ")";
+		canvas.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+	}
+	
+	override public function drawRect(x :Float, y : Float, width : Float, height : Float) {
+		canvas.rect(tx + x, ty + y, width, height);
+	}
+	
+	override public function fillRect(x : Float, y : Float, width : Float, height : Float) {
+		canvas.fillRect(tx + x, ty + y, width, height);
+	}
+
+	override public function drawString(text : String, x : Float, y : Float) {
+		canvas.fillText(text, tx + x, ty + y);
+	}
+
+	override public function setFont(font : com.ktxsoftware.kha.Font) {
+		var webfont = cast(font, Font);
+		canvas.setFont(webfont.size + "px " + webfont.name);
+	}
+
+	override public function drawLine(x1 : Float, y1 : Float, x2 : Float, y2 : Float) {
+		canvas.moveTo(tx + x1, ty + y1);
+		canvas.lineTo(tx + x2, ty + y2);
+		canvas.moveTo(0, 0);
+	}
+
+	override public function fillTriangle(x1 : Float, y1 : Float, x2 : Float, y2 : Float, x3 : Float, y3 : Float) {
+		canvas.beginPath();
+		
+		canvas.closePath();
+		canvas.fill();
 	}
 }
