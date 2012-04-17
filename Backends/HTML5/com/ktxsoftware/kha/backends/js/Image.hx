db000b66ce7d759b00c05872639bfda6d1516f16
Robert Konrad <robert@ktx-software.com>
2012-04-17 10:50:34 +0200

    Activated bilinear filtering in WebGL. Made HTML5 images more efficient.

--- Backends/HTML5/com/ktxsoftware/kha/backends/js/Image.hx
+++ Backends/HTML5/com/ktxsoftware/kha/backends/js/Image.hx
@@ -20,13 +20,6 @@ class Image implements com.ktxsoftware.kha.Image {
 	
 	public function new(image : js.Image) {
 		this.image = image;
-		if (context != null) {
-			context.strokeStyle = "rgb(255,255,0)";
-			context.fillStyle = "rgb(255,255,0)";
-			context.fillRect(0, 0, image.width, image.height);
-			context.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
-			data = context.getImageData(0, 0, image.width, image.height);
-		}
 	}
 	
 	public function getWidth() : Int {
@@ -38,9 +31,20 @@ class Image implements com.ktxsoftware.kha.Image {
 	}
 	
 	public function isAlpha(x : Int, y : Int) : Bool {
-		if (data == null) return true;
+		if (data == null) {
+			if (context == null) return true;
+			else createImageData();
+		}
 		var r = data.data[y * image.width * 4 + x * 4 + 0];
 		var g = data.data[y * image.width * 4 + x * 4 + 1];
 		return !(data.data[y * image.width * 4 + x * 4 + 0] == 255 && data.data[y * image.width * 4 + x * 4 + 1] == 255);
+	}
+	
+	function createImageData() {
+		context.strokeStyle = "rgb(255,255,0)";
+		context.fillStyle = "rgb(255,255,0)";
+		context.fillRect(0, 0, image.width, image.height);
+		context.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
+		data = context.getImageData(0, 0, image.width, image.height);
 	}
 }
--- Backends/HTML5/com/ktxsoftware/kha/backends/js/PainterGL.hx
+++ Backends/HTML5/com/ktxsoftware/kha/backends/js/PainterGL.hx
@@ -212,11 +212,14 @@ class PainterGL extends com.ktxsoftware.kha.Painter {
 			gl.activeTexture(gl.TEXTURE0);
 			gl.bindTexture(gl.TEXTURE_2D, img.tex);
 			//glContext.pixelStorei(WebGLRenderingContext.UNPACK_FLIP_Y_WEBGL, 1);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+ 
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR); //gl.NEAREST
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR); //gl.NEAREST
+			
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img.image);
+			//gl.generateMipmap(gl.TEXTURE_2D);
 			gl.uniform1i(textureUniform, 0);
 		}
 		else gl.bindTexture(gl.TEXTURE_2D, img.tex);
