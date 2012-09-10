bc22a8fd427868d66839288004463b4209fd79fc
creuter <christian.reuter@kom.tu-darmstadt.de>
2012-09-10 09:54:34 +0200

    Made wpf image more error resilient.

--- Backends/WPF/kha/wpf/Image.hx
+++ Backends/WPF/kha/wpf/Image.hx
@@ -27,6 +27,9 @@ class Image implements kha.Image {
 	}
 	
 	@:functionBody('
+		if (x < 0 || y < 0 || x >= image.PixelWidth || y >= image.PixelHeight)
+            return false;
+		
 		byte[] pixels = new byte[8];
 		image.CopyPixels(new System.Windows.Int32Rect(x, y, 1, 1), pixels, image.PixelWidth * 4, 0);
 		return pixels[0] > 0;
