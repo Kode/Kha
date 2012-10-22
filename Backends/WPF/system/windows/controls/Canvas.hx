5e4c9fd5e78bf3c55709141e60a9d7509ae1517c
Robert Konrad <robert@ktx-software.com>
2012-10-22 22:26:55 +0200

    Made System.Windows.Controls.Canvas extern in WPF backend.

--- Backends/WPF/system/windows/controls/Canvas.hx
+++ Backends/WPF/system/windows/controls/Canvas.hx
@@ -1,8 +1,8 @@
 package system.windows.controls;
+
 import system.windows.FrameworkElement;
 
 @:native("System.Windows.Controls.Canvas")
-class Canvas extends FrameworkElement
-{
-	public function new() { }
+extern class Canvas extends FrameworkElement {
+	public function new(): Void;
 }
