066ce2dc7c80dc4817fd73c9751369cd6a6df657
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2012-10-26 18:36:17 +0200

    fix for Flash texture limit

--- Backends/Flash/kha/flash/Image.hx
+++ Backends/Flash/kha/flash/Image.hx
@@ -9,6 +9,8 @@ import flash.display3D.textures.Texture;
 import flash.geom.Matrix;
 
 class Image implements kha.Image {
+	static var maxTextureControll : List<Image> = new List<Image>();
+	
 	public var image : Bitmap;
 	var tex : Texture;
 	var texWidth : Int;
@@ -16,6 +18,13 @@ class Image implements kha.Image {
 	
 	public function new(image : DisplayObject)  {
 		this.image = cast(image, Bitmap);
+	}
+	
+	public function dispose() : Void {
+		if (tex != null) {
+			tex.dispose();
+			tex = null;
+		}
 	}
 	
 	public function getWidth() : Int {
@@ -50,10 +59,19 @@ class Image implements kha.Image {
 		return -1;
     }
 	
-	function uploadTextureWithMipmaps() : Void {
+	function uploadTextureWithMipmaps() : Void {		
 		texWidth = toPow2(Std.int(image.width));
 		texHeight = toPow2(Std.int(image.height));
-		tex = Starter.context.createTexture(texWidth, texHeight, Context3DTextureFormat.BGRA, false);
+		while (tex == null) {
+			try {
+				tex = Starter.context.createTexture(texWidth, texHeight, Context3DTextureFormat.BGRA, false);
+				maxTextureControll.add(this);
+			}
+			catch (e : Dynamic) {
+				var toDispose = maxTextureControll.pop();
+				toDispose.dispose();
+			}
+		}
 		tex.uploadFromBitmapData(image.bitmapData, 0);
 		
 		/*var level : Int = 0;
