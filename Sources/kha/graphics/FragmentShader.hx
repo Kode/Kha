35dcf7977fe017612300129dce3924274277cf80
Robert Konrad <robert@ktx-software.com>
2012-12-10 10:19:46 +0100

    Added float3 shader parameters.

--- Sources/kha/graphics/FragmentShader.hx
+++ Sources/kha/graphics/FragmentShader.hx
@@ -4,4 +4,5 @@ interface FragmentShader {
 	function setInt(name: String, value: Int): Void;
 	function setFloat(name: String, value: Float): Void;
 	function setFloat2(name: String, value1: Float, value2: Float): Void;
+	function setFloat3(name: String, value1: Float, value2: Float, value3: Float): Void;
 }
