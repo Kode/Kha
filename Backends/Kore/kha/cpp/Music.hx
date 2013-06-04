c3fe4d3550e28173e09d3537c4090fcd700a7c02
Robert Konrad <robert@ktx-software.com>
2013-06-04 23:35:10 +0200

    Added Music support to Kore backend.

--- Backends/Kore/kha/cpp/Music.hx
+++ Backends/Kore/kha/cpp/Music.hx
@@ -2,22 +2,23 @@ package kha.cpp;
 
 @:headerCode('
 #include <Kore/pch.h>
-#include <Kore/Audio/Sound.h>
+#include <Kore/Audio/SoundStream.h>
+#include <Kore/Audio/Mixer.h>
 ')
 
-//@:headerClassCode("Kt::Text music;")
+@:headerClassCode("Kore::SoundStream* stream;")
 class Music extends kha.Music {
 	public function new(filename : String) {
 		super();
-		loadMusic(filename);
+		loadMusic(filename + ".ogg");
 	}
 	
-	//@:functionCode("music = Kt::Text(filename.c_str());")
+	@:functionCode("stream = new Kore::SoundStream(filename.c_str(), true);")
 	function loadMusic(filename : String) {
 		
 	}
 	
-	//@:functionCode("Kt::Music::play(music);")
+	@:functionCode("Kore::Mixer::play(stream);")
 	override public function play() : Void {
 		
 	}
