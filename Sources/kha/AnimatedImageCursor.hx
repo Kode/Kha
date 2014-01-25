55b313d8b0cc4e4259bd955110b39f55c9175628
Robert Konrad <robert@ktx-software.com>
2014-01-25 11:57:21 +0100

    Added click point to cursors.

--- Sources/kha/AnimatedImageCursor.hx
+++ Sources/kha/AnimatedImageCursor.hx
@@ -5,8 +5,10 @@ class AnimatedImageCursor implements Cursor {
 	private var width: Int;
 	private var height: Int;
 	private var animation: Animation;
+	private var clickX: Int;
+	private var clickY: Int;
 	
-	public function new(image: Image, width: Int, height: Int, animation: Animation) {
+	public function new(image: Image, width: Int, height: Int, animation: Animation, clickX: Int, clickY: Int) {
 		this.image = image;
 		this.width = width;
 		this.height = height;
@@ -15,7 +17,7 @@ class AnimatedImageCursor implements Cursor {
 	}
 	
 	public function render(painter: Painter, x: Int, y: Int): Void {
-		painter.drawImage2(image, Std.int(animation.get() * width) % image.width, Math.floor(animation.get() * width / image.width) * height, width, height, x, y, width, height);
+		painter.drawImage2(image, Std.int(animation.get() * width) % image.width, Math.floor(animation.get() * width / image.width) * height, width, height, x - clickX, y - clickY, width, height);
 	}
 	
 	public function update(): Void {
--- Sources/kha/ImageCursor.hx
+++ Sources/kha/ImageCursor.hx
@@ -2,13 +2,17 @@ package kha;
 
 class ImageCursor implements Cursor {
 	private var image: Image;
+	private var clickX: Int;
+	private var clickY: Int;
 
-	public function new(image: Image) {
+	public function new(image: Image, clickX: Int, clickY: Int) {
 		this.image = image;
+		this.clickX = clickX;
+		this.clickY = clickY;
 	}
 	
 	public function render(painter: Painter, x: Int, y: Int): Void {
-		painter.drawImage(image, x, y);
+		painter.drawImage(image, x - clickX, y - clickY);
 	}
 	
 	public function update(): Void {
