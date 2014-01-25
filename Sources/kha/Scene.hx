9393d702ea14cad1d4a5f50efc2aa4ddad89c371
Ingo Jakobs <ingo.jakobs@stud.tu-darmstadt.de>
2014-01-25 16:56:29 +0100

    another helper

--- Sources/kha/Scene.hx
+++ Sources/kha/Scene.hx
@@ -187,7 +187,7 @@ class Scene {
 	
 	function sort(sprites : Array<Sprite>) {
 		if (sprites.length == 0) return;
-		sprites.sort(function(arg0: Sprite, arg1: Sprite) {
+		ArraySort.sort(sprites, function(arg0: Sprite, arg1: Sprite) {
 			if (arg0.x < arg1.x) return -1;
 			else if (arg0.x == arg1.x) return 0;
 			else return 1;
@@ -294,5 +294,23 @@ class Scene {
 		}
 		ArraySort.sort(heroes, function(h1 : Sprite, h2 : Sprite) : Int { if (h1.z == h2.z) return 0; else if (h1.z < h2.z) return 1; else return -1; } );
 		return heroes;
+	}
+	
+	public function getSpritesBelowPoint(px : Int, py : Int) : Array<Sprite> {
+		var sprites = new Array();
+		
+		for (i in 1...this.sprites.length+1) {
+			var sprite = this.sprites[this.sprites.length - i];
+			if (sprite.x + sprite.width < px)
+				continue;
+			if (sprite.x > px)
+				break;
+			var rect = sprite.collisionRect();
+			if (rect.x < px && px < rect.x + rect.width && rect.y < py && py < rect.y + rect.height) {
+				sprites.push(sprite);
+			}
+		}
+		ArraySort.sort(sprites, function(h1 : Sprite, h2 : Sprite) : Int { if (h1.z == h2.z) return 0; else if (h1.z < h2.z) return 1; else return -1; } );
+		return sprites;
 	}
 }
