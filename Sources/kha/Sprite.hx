a3bf4399ffb4065d7460021616a6879c3cfa2e3f
Robert Konrad <robert@ktx-software.com>
2014-07-17 00:08:08 +0200

    Readded rotation support for Sprites.

--- Sources/kha/Sprite.hx
+++ Sources/kha/Sprite.hx
@@ -66,9 +66,10 @@ class Sprite {
 	public function render(g: Graphics): Void {
 		if (image != null) {
 			g.color = Color.White;
-			g.pushTransformation(Matrix3.rotation(0.1) * g.transformation);
+			var rotated = rotation != null && rotation.angle != 0;
+			if (rotated) g.pushTransformation(g.transformation * Matrix3.translation(x + rotation.center.x, y + rotation.center.y) * Matrix3.rotation(0.1) * Matrix3.translation(-x - rotation.center.x, -y - rotation.center.y));
 			g.drawScaledSubImage(image, Std.int(animation.get() * w) % image.width, Math.floor(animation.get() * w / image.width) * h, w, h, Math.round(x - collider.x * scaleX), Math.round(y - collider.y * scaleY), width, height);// , rotation);
-			g.popTransformation();
+			if (rotated) g.popTransformation();
 		}
 		#if debug
 			g.color = Color.fromBytes(255, 0, 0);
