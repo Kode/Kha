6a4ca27f0eb546029b7f5c0b16b79c558a96e59b
Robert Konrad <robert@ktx-software.com>
2014-08-21 19:06:00 +0200

    Fixed text rendering in Flash.

--- Backends/Flash/kha/Image.hx
+++ Backends/Flash/kha/Image.hx
@@ -29,7 +29,7 @@ class Image implements Canvas implements Resource {
 	private var graphics4: kha.graphics4.Graphics;
 	
 	public static function create(width: Int, height: Int, format: TextureFormat = null, usage: Usage = null, levels: Int = 1): Image {
-		return null;
+		return new Image(width, height, format == null ? TextureFormat.RGBA32 : format, false, false, false);
 	}
 	
 	public static function createRenderTarget(width: Int, height: Int, format: TextureFormat = null, depthStencil: Bool = false, antiAliasingSamples: Int = 1): Image {
