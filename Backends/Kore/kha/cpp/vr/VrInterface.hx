c39c11345aaf20531beb2c0b0c1d5d1ed5f59d0f
Florian Mehm <florian@mehm.net>
2015-03-17 10:55:44 +0100

    Can now switch between windows and android build without copying any files.

--- Backends/Kore/kha/cpp/vr/VrInterface.hx
+++ Backends/Kore/kha/cpp/vr/VrInterface.hx
@@ -3,22 +3,29 @@ package kha.cpp.vr;
 import kha.vr.SensorState;
 import kha.vr.TimeWarpParms;
 
+#if ANDROID
+
 @:headerCode('
 #include <Kore/pch.h>
 #include <Kore/Vr/VrInterface.h>
 ')
+
+#end 
+
 class VrInterface extends kha.vr.VrInterface {
 	
-	
-	
+	#if ANDROID
 	// Returns the current sensor state
 	// Returns the predicted sensor state at the specified time
 	@:functionCode('
 		return Kore::VrInterface::GetSensorState();	
 	')
+	
+
 	public override function GetSensorState(): SensorState {
 		return null;
 	}
+	
 	
 	// Returns the predicted sensor state at the specified time
 	@:functionCode('
@@ -59,7 +66,46 @@ class VrInterface extends kha.vr.VrInterface {
 		return 0.0;
 	}
 	
+	#else 
 	
+	// Returns the current sensor state
+	// Returns the predicted sensor state at the specified time
+	
+	public override function GetSensorState(): SensorState {
+		return null;
+	}
+	
+	
+	// Returns the predicted sensor state at the specified time
+	
+	public override function GetPredictedSensorState(time: Float): SensorState {
+		return null;
+	}
+	
+	// Sends a black image to the warp swap thread
+	
+	public override function WarpSwapBlack(): Void {
+		
+	}
+	
+	
+	// Sends the Oculus loading symbol to the warp swap thread
+	
+	public override function WarpSwapLoadingIcon(): Void {
+		
+	}
+	
+	// Sends the set of images to the warp swap thread
+	
+	public override function WarpSwap(parms: TimeWarpParms): Void {
+	}
+	
+	
+	public override function GetTimeInSeconds(): Float {
+		return 0.0;
+	}
+	
+	#end
 	
 	public function new() {
 		super();
