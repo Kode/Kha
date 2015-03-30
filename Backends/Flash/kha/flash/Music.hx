6d063014771b4350692983c4dcf2796146988db3
creuter <christian.reuter@kom.tu-darmstadt.de>
2015-03-30 16:33:08 +0200

    Fixed neverending music in flash.

--- Backends/Flash/kha/flash/Music.hx
+++ Backends/Flash/kha/flash/Music.hx
@@ -1,17 +1,23 @@
 package kha.flash;
+import flash.events.Event;
 
 class Music extends kha.Music {
 	private var music: flash.media.Sound;
 	private var channel: flash.media.SoundChannel;
-
+	private var running : Bool;
+	
 	public function new(music: flash.media.Sound) {
 		super();
 		this.music = music;
+		
+		running = false;
 	}
 	
 	public override function play(loop: Bool = false): Void {
 		if (channel != null) channel.stop();
+		running = true;
 		channel = music.play(0, loop ? 1000 * 1000 * 100 : 1);
+		channel.addEventListener(Event.SOUND_COMPLETE, function(_) { running = false; } );
 	}
 	
 	public override function stop(): Void {
@@ -24,5 +30,9 @@ class Music extends kha.Music {
 	
 	override public function getLength(): Int {
 		return Std.int(music.length * 10) * 10; // FIXME! there seems to be a timing issue.
+	}
+	
+	override public function isFinished() : Bool {
+		return !running;
 	}
 }
