e59dbd848af1b9793051ce11ceb2550208ab0c86
Robert Konrad <robert@ktx-software.com>
2015-04-13 14:53:09 +0200

    More SuperString methods.

--- Sources/kha/SuperString.hx
+++ Sources/kha/SuperString.hx
@@ -2,6 +2,8 @@ package kha;
 
 import haxe.Utf8;
 
+using StringTools;
+
 // To be deleted when Haxe gets UCS2 support
 
 #if cpp
@@ -46,6 +48,36 @@ abstract SuperString(String) {
 			}
 		});
 		return new SuperString(buffer.toString());
+	}
+
+	public function toLowerCase(): SuperString {
+		var buffer = new StringBuf();
+		Utf8.iter(this, function (code: Int) {
+			if (code >= 'A'.charCodeAt(0) && code <= 'Z'.charCodeAt(0)) {
+				buffer.addChar(code - 'A'.charCodeAt(0) + 'a'.charCodeAt(0));
+			}
+			else if (code == Utf8.charCodeAt('Ä', 0)) {
+				buffer.addChar(Utf8.charCodeAt('ä', 0));
+			}
+			else if (code == Utf8.charCodeAt('Ö', 0)) {
+				buffer.addChar(Utf8.charCodeAt('ö', 0));
+			}
+			else if (code == Utf8.charCodeAt('Ü', 0)) {
+				buffer.addChar(Utf8.charCodeAt('ü', 0));
+			}
+			else {
+				buffer.addChar(code);
+			}
+		});
+		return new SuperString(buffer.toString());
+	}
+
+	public function trim(): SuperString {
+		return new SuperString(StringTools.trim(this));
+	}
+
+	public function substr(pos: Int, ?len: Int): SuperString {
+		return new SuperString(Utf8.sub(this, pos, len == null ? length - pos : len));
 	}
 	
 	public function toString(): String {
