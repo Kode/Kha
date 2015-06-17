f8cfd20db1e60ad8228fae5b09c54f1d6333cf5f
Robert Konrad <robert@ktx-software.com>
2015-06-17 23:46:42 +0200

    Added audio element based Audio1 implementation to HTML5 backend.

---
+++ Backends/HTML5/kha/js/AEMusicChannel.hx
@@ -0,0 +1,71 @@
+package kha.js;
+
+import js.html.AudioElement;
+import kha.audio1.MusicChannel;
+
+class AEMusicChannel {
+	private var element: AudioElement;
+	private var loop: Bool;
+	
+	public function new(music: Music, loop: Bool) {
+		this.element = music.element;
+		this.loop = loop;
+	}
+		
+	public function play(): Void {
+		element.loop = loop;
+		element.play();
+	}
+
+	public function pause(): Void {
+		try {
+			element.pause();
+		}
+		catch (e: Dynamic) {
+			trace(e);
+		}
+	}
+
+	public function stop(): Void {
+		try {
+			element.pause();
+			element.currentTime = 0;
+		}
+		catch (e: Dynamic) {
+			trace(e);
+		}
+	}
+
+	public var length(get, null): Int; // Miliseconds
+	
+	private function get_length(): Int {
+		if (Math.isFinite(element.duration)) {
+			return Math.floor(element.duration * 1000); // Miliseconds
+		}
+		else {
+			return -1;
+		}
+	}
+
+	public var position(get, null): Int; // Miliseconds
+	
+	private function get_position(): Int {
+		return Math.ceil(element.currentTime * 1000);  // Miliseconds
+	}
+
+	public var volume(get, set): Float;
+	
+	private function get_volume(): Float {
+		return 1;
+	}
+
+	private function set_volume(value: Float): Float {
+		return 1;
+	}
+
+	public var finished(get, null): Bool;
+	
+	private function get_finished(): Bool {
+		return position >= length;
+	}
+}
--- Backends/HTML5/kha/js/AudioElementAudio.hx
+++ Backends/HTML5/kha/js/AudioElementAudio.hx
@@ -5,10 +5,13 @@ class AudioElementAudio {
 	public static function _compile(): Void { }
 	
 	public static function playSound(sound: Sound): kha.audio1.SoundChannel {
-		return null;
+		sound.element.play();
+		return cast new AESoundChannel(sound);
 	}
 
 	public static function playMusic(music: Music, loop: Bool = false): kha.audio1.MusicChannel {
-		return null;
+		music.element.loop = loop;
+		music.element.play();
+		return cast new AEMusicChannel(music, loop);
 	}
 }
