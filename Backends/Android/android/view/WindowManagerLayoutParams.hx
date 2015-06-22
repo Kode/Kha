b3eb65106dd2232a38671585369de801e2f7b6c9
Robert Konrad <robert@ktx-software.com>
2015-06-22 08:41:06 +0200

    Dalvik compilation fix.

--- Backends/Android/android/view/WindowManagerLayoutParams.hx
+++ Backends/Android/android/view/WindowManagerLayoutParams.hx
@@ -1,6 +1,6 @@
 package android.view;
 
-@:native("android.view.View.WindowManager.LayoutParams")
+@:native("android.view.WindowManager.LayoutParams")
 extern class WindowManagerLayoutParams {
 	public static var FLAG_FULLSCREEN: Int;
 }
