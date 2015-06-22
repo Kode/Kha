42bf3a4379b5ffaa6734e967c9256111d518ff23
Robert Konrad <robert@ktx-software.com>
2015-06-22 10:43:34 +0200

    Implemented Android shaders.

---
+++ Backends/Android/kha/android/graphics4/TextureUnit.hx
@@ -0,0 +1,9 @@
+package kha.android.graphics4;
+
+class TextureUnit implements kha.graphics4.TextureUnit {
+	public var value: Int;
+	
+	public function new(value: Int) {
+		this.value = value;
+	}
+}
