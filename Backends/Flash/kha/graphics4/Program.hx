ca3a99dd3986aef86bdce855277241b50c32d281
Robert Konrad <robert@ktx-software.com>
2015-11-10 00:05:30 +0100

    Made Flash backend much faster.

--- Backends/Flash/kha/graphics4/Program.hx
+++ Backends/Flash/kha/graphics4/Program.hx
@@ -14,6 +14,9 @@ class Program {
 	private var fragmentShader: FragmentShader;
 	private var vertexShader: VertexShader;
 	
+	private var vc: haxe.ds.Vector<flash.Vector<Float>>;
+	private var fc: haxe.ds.Vector<flash.Vector<Float>>;
+	
 	public function new() {
 		program = kha.flash.graphics4.Graphics.context.createProgram();
 	}
@@ -27,6 +30,48 @@ class Program {
 	}
 	
 	public function link(structure: VertexStructure): Void {
+		var vclength: Int = 0;
+		for (i in 0...128) {
+			var name = "vc" + i;
+			if (Reflect.hasField(vertexShader.constants, name)) {
+				vclength = i + 1;
+			}
+		}
+		
+		var fclength: Int = 0;
+		for (i in 0...28) {
+			var name = "fc" + i;
+			if (Reflect.hasField(fragmentShader.constants, name)) {
+				fclength = i + 1;
+			}
+		}
+	
+		vc = new haxe.ds.Vector<flash.Vector<Float>>(vclength);
+		for (i in 0...vclength) {
+			var name = "vc" + i;
+			if (Reflect.hasField(vertexShader.constants, name)) {
+				var field = Reflect.field(vertexShader.constants, name);
+				vc[i] = new flash.Vector<Float>(4);
+				vc[i][0] = field[0];
+				vc[i][1] = field[1];
+				vc[i][2] = field[2];
+				vc[i][3] = field[3];
+			}
+		}
+
+		fc = new haxe.ds.Vector<flash.Vector<Float>>(fclength);
+		for (i in 0...fclength) {
+			var name = "fc" + i;
+			if (Reflect.hasField(fragmentShader.constants, name)) {
+				var field = Reflect.field(fragmentShader.constants, name);
+				fc[i] = new flash.Vector<Float>(4);
+				fc[i][0] = field[0];
+				fc[i][1] = field[1];
+				fc[i][2] = field[2];
+				fc[i][3] = field[3];
+			}
+		}
+		
 		var vertexAssembler = new AGALMiniAssembler();
 		vertexAssembler.assemble(Context3DProgramType.VERTEX, vertexShader.source);
 		
@@ -64,27 +109,15 @@ class Program {
 	public function set(): Void {
 		kha.flash.graphics4.Graphics.context.setProgram(program);
 		
-		var vec = new flash.Vector<Float>(4);
-		for (i in 0...128) {
-			var name = "vc" + i;
-			if (Reflect.hasField(vertexShader.constants, name)) {
-				var field = Reflect.field(vertexShader.constants, name);
-				vec[0] = field[0];
-				vec[1] = field[1];
-				vec[2] = field[2];
-				vec[3] = field[3];
-				kha.flash.graphics4.Graphics.context.setProgramConstantsFromVector(Context3DProgramType.VERTEX, i, vec);
+		for (i in 0...vc.length) {
+			if (vc[i] != null) {
+				kha.flash.graphics4.Graphics.context.setProgramConstantsFromVector(Context3DProgramType.VERTEX, i, vc[i]);
 			}
 		}
-		for (i in 0...28) {
-			var name = "fc" + i;
-			if (Reflect.hasField(fragmentShader.constants, name)) {
-				var field = Reflect.field(fragmentShader.constants, name);
-				vec[0] = field[0];
-				vec[1] = field[1];
-				vec[2] = field[2];
-				vec[3] = field[3];
-				kha.flash.graphics4.Graphics.context.setProgramConstantsFromVector(Context3DProgramType.FRAGMENT, i, vec);
+		
+		for (i in 0...fc.length) {
+			if (fc[i] != null) {
+				kha.flash.graphics4.Graphics.context.setProgramConstantsFromVector(Context3DProgramType.FRAGMENT, i, fc[i]);
 			}
 		}
 	}
