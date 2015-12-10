cf9811ee67ee5873aad9c9828ad1b0a333c7f5ff
Robert Konrad <robert@ktx-software.com>
2015-12-10 19:32:49 +0100

    Use video format returned from Kore.

--- Backends/Kore/kha/LoaderImpl.hx
+++ Backends/Kore/kha/LoaderImpl.hx
@@ -42,8 +42,13 @@ class LoaderImpl {
 		done(new kha.kore.Video(desc.files[0]));
 	}
 	
+	@:functionCode('return Kore::System::videoFormats()[0];')
+	private static function videoFormat(): String {
+		return "";
+	}
+	
 	public static function getVideoFormats(): Array<String> {
-		return ["mp4"];
+		return [videoFormat()];
 	}
 
 	@:functionCode('Kore::System::showKeyboard();')
