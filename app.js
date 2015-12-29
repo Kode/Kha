932093df1ea815d4821121d2bca461626ef67647
Otavio Pliger <otavio.pliger@outlook.it>
2015-12-29 00:57:32 +0000

    Major update: from static pages to EJS

--- app.js
+++ app.js
@@ -15,15 +15,42 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page 
+app.get('/', function(req, res) {
+
+    res.render('pages/index');
+});
+
+// download page 
+app.get('/download', function(req, res) {
+
+    res.render('pages/download');
+});
+
+// getstarted -> install page 
+app.get('/getstarted/install', function(req, res) {
+
+    res.render('pages/install');
+});
+
+// getstarted -> create page 
+app.get('/getstarted/create', function(req, res) {
+
+    res.render('pages/create');
+});
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, appEnv.bind, function() {
 
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
 });
