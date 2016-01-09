defda8e7fc4ad489ee7898d62d9337b7f6978055
Otavio Pliger <Ota@Otavios-MacBook-Air.local>
2016-01-09 03:24:40 +0000

    Moved from IBM Bluemix to RedHat OpenShift

--- server.js
+++ server.js
@@ -8,15 +8,8 @@
 // for more info, see: http://expressjs.com
 var express = require('express');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
 // create a new express server
 var app = express();
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -54,9 +47,12 @@ app.get('/getstarted/build', function(req, res) {
     res.render('pages/build');
 });
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 // start server on the specified port and binding host
-app.listen(appEnv.port, appEnv.bind, function() {
+app.listen(app.get('port'), app.get('ip'), function() {
 
     // print a message when the server starts listening
-    console.log("server starting on " + appEnv.url);
+    console.log("✔ Express server listening at %s:%d ", app.get('ip'),app.get('port'));
 });
