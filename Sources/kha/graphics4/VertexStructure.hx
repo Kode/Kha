16064ad9c4e4cbf9709b9c4119985270e4afc1c5
Christian Reuter <christian@aoe-maniac.de>
2016-01-16 19:21:00 +0100

    Vertex structures can now calculate their byte size, so no more need to calculate it manually.

--- Sources/kha/graphics4/VertexStructure.hx
+++ Sources/kha/graphics4/VertexStructure.hx
@@ -15,6 +15,32 @@ class VertexStructure {
 		return elements.length;
 	}
 	
+	public function byteSize(): Int {
+		var byteSize = 0;
+		
+		for (i in 0...elements.length) {
+			byteSize += dataByteSize(elements[i].data);
+		}
+		
+		return byteSize;
+	}
+	
+	private function dataByteSize(data: VertexData) : Int {
+		switch(data) {
+			case Float1:
+				return 1;
+			case Float2:
+				return 2;
+			case Float3:
+				return 3;
+			case Float4:
+				return 4;
+			case Float4x4:
+				return 16;
+		}
+		return 0;
+	}
+	
 	public function get(index: Int): VertexElement {
 		return elements[index];
 	}
