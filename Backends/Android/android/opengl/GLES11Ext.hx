084be19b6af40a2d06d63c131c0c128a812e6dde
David Klein <dave@smokinheads.de>
2016-02-08 11:04:29 +0100

    Android: Support for depth/stencil buffer

---
+++ Backends/Android/android/opengl/GLES11Ext.hx
@@ -0,0 +1,7 @@
+package android.opengl;
+
+class GLES11Ext {
+	public static var GL_DEPTH_STENCIL_OES: Int;
+	public static var GL_DEPTH24_STENCIL8_OES: Int;
+	public static var GL_UNSIGNED_INT_24_8_OES: Int;
+}
