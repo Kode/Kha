8251307af2aa2b6904fd6163b28cdad4a3813aa5
Matt <magneticrealms@users.noreply.github.com>
2016-02-20 22:51:05 +0900

    Reverting unnecessary changes to AudioChannel.

--- Sources/kha/audio1/AudioChannel.hx
+++ Sources/kha/audio1/AudioChannel.hx
@@ -5,12 +5,12 @@ interface AudioChannel {
 	public function pause(): Void;
 	public function stop(): Void;
 	public var length(get, null): Float; // Seconds
-	public function get_length(): Float;
+	private function get_length(): Float;
 	public var position(get, null): Float; // Seconds
-	public function get_position(): Float;
+	private function get_position(): Float;
 	public var volume(get, set): Float;
-	public function get_volume(): Float;
-	public function set_volume(value: Float): Float;
+	private function get_volume(): Float;
+	private function set_volume(value: Float): Float;
 	public var finished(get, null): Bool;
-	public function get_finished(): Bool;
+	private function get_finished(): Bool;
 }
