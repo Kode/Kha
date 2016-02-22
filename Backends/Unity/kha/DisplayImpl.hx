d41499dea67943d68808f95e33f10ec1ac232e28
David Klein <dave@smokinheads.de>
2016-02-22 15:28:50 +0100

    dummy DisplayImpl for unity

---
+++ Backends/Unity/kha/DisplayImpl.hx
@@ -0,0 +1,15 @@
+package kha;
+
+class DisplayImpl {
+	public static function count() : Int {
+		return 1;
+	}
+
+	public static function width( index : Int ) : Int {
+		throw 'TODO (DK) implement me';
+	}
+
+	public static function height( index : Int ) : Int {
+		throw 'TODO (DK) implement me';
+	}
+}
