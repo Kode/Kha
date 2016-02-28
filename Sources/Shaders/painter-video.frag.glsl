20d060d0d642e1ae3d59f13dfee26cde2d999c30
Robert Konrad <robert@ktx-software.com>
2016-02-28 22:08:29 +0100

    Temporarily removed samplerVideo.

--- Sources/Shaders/painter-video.frag.glsl
+++ Sources/Shaders/painter-video.frag.glsl
@@ -2,7 +2,7 @@
 #extension GL_ARB_separate_shader_objects : enable
 #extension GL_ARB_shading_language_420pack : enable
 
-layout(binding = 2) uniform samplerVideo tex;
+layout(binding = 2) uniform sampler2D tex;
 layout(location = 0) in vec2 texCoord;
 layout(location = 1) in vec4 color;
 
