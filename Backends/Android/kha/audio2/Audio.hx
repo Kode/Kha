e43c05e3a1f4616f59a6c50c82618212b698f338
David Klein <dave@smokinheads.de>
2016-04-01 14:50:09 +0200

    null implementation of Audio2 so my project compiles on android java target

---
+++ Backends/Android/kha/audio2/Audio.hx
@@ -0,0 +1,12 @@
+package kha.audio2;
+
+class Audio {
+	@:noCompletion
+	public static function _init(): Bool {
+		return false;
+	}
+
+	public static function play(sound: Sound, loop: Bool = false): kha.audio1.AudioChannel {
+		return null;
+	}
+}
