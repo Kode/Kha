ce27e3e37fef2a53d06a5aaff5ae9ce352ffe209
Robert Konrad <git2016@robdangero.us>
2016-07-16 18:53:16 +0200

    Made html5 Network startup more robust.

--- Backends/HTML5/kha/network/Network.hx
+++ Backends/HTML5/kha/network/Network.hx
@@ -6,14 +6,18 @@ import js.html.WebSocket;
 
 class Network {
 	private var socket: WebSocket;
+	private var open: Bool = false;
 	
 	public function new(url: String, port: Int) {
 		socket = new WebSocket("ws://" + url + ":" + port);
 		socket.binaryType = BinaryType.ARRAYBUFFER;
+		socket.onopen = function () {
+			open = true;
+		};
 	}
 	
 	public function send(bytes: Bytes, mandatory: Bool): Void {
-		socket.send(bytes.getData());
+		if (open) socket.send(bytes.getData());
 	}
 	
 	public function listen(listener: Bytes->Void): Void {
