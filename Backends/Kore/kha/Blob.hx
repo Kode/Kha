596ce3fa3f19f5aad0dd0176a2f842424a531b89
Lubos Lenco <lubos.lenco@gmail.com>
2016-07-20 18:21:14 +0200

    Blob.toBytes() for Kore.

--- Backends/Kore/kha/Blob.hx
+++ Backends/Kore/kha/Blob.hx
@@ -166,6 +166,10 @@ class Blob {
 		if (bytes.get(0) == 239 && bytes.get(1) == 187 && bytes.get(2) == 191) return bytes.sub(3, bytes.length - 3).toString();
 		else return bytes.toString();
 	}
+
+	public function toBytes(): Bytes {
+		return bytes;
+	}
 		
 	public function unload(): Void {
 		bytes = null;
