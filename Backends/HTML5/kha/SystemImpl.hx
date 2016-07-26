45c0925973277629fc2f833bf6a864d7af19a551
Robert Konrad <git2016@robdangero.us>
2016-07-27 00:56:00 +0200

    Move HTML5 mouse cursor code to the correct place

--- Backends/HTML5/kha/SystemImpl.hx
+++ Backends/HTML5/kha/SystemImpl.hx
@@ -168,7 +168,7 @@ class SystemImpl {
 	public static function init2(?backbufferFormat: TextureFormat) {
 		haxe.Log.trace = untyped js.Boot.__trace; // Hack for JS trace problems
 		keyboard = new Keyboard();
-		mouse = new kha.input.Mouse();
+		mouse = new kha.input.MouseImpl();
 		surface = new Surface();
 		gamepads = new Array<Gamepad>();
 		gamepadStates = new Array<GamepadStates>();
@@ -249,6 +249,7 @@ class SystemImpl {
 
 	private static function loadFinished() {
 		var canvas: Dynamic = Browser.document.getElementById("khanvas");
+		canvas.style.cursor = "default";
 
 		var gl: Bool = false;
 
--- Backends/HTML5/kha/input/MouseImpl.hx
+++ Backends/HTML5/kha/input/MouseImpl.hx
@@ -4,4 +4,12 @@ class MouseImpl extends kha.input.Mouse {
 	public function new() {
 		super();
 	}
+
+	override public function hideSystemCursor(): Void {
+		SystemImpl.khanvas.style.cursor = "none";
+	}
+
+	override public function showSystemCursor(): Void {
+		SystemImpl.khanvas.style.cursor = "default";
+	}
 }
