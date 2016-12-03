830eb9a96cb2b9a333a46aafc5d103c67ae2246b
Robert Konrad <git2016@robdangero.us>
2016-12-03 18:43:41 +0100

    Fix #456

--- Backends/HTML5/kha/js/AudioElementAudio.hx
+++ Backends/HTML5/kha/js/AudioElementAudio.hx
@@ -5,7 +5,11 @@ class AudioElementAudio {
 	@:noCompletion
 	public static function _compile(): Void { }
 	
-	public static function play(sound: Sound, loop: Bool = false, stream: Bool = false): kha.audio1.AudioChannel {
+	public static function play(sound: Sound, loop: Bool = false): kha.audio1.AudioChannel {
+		return stream(sound, loop);
+	}
+
+	public static function stream(sound: Sound, loop: Bool = false): kha.audio1.AudioChannel {
 		sound.element.loop = loop;
 		var channel = new AEAudioChannel(sound.element);
 		channel.play();
--- Sources/kha/audio1/Audio.hx
+++ Sources/kha/audio1/Audio.hx
@@ -3,7 +3,7 @@ package kha.audio1;
 import kha.Sound;
 
 extern class Audio {
-	public static function play(sound: Sound, loop: Bool = false, stream: Bool = false): AudioChannel;
+	public static function play(sound: Sound, loop: Bool = false): AudioChannel;
 	
 	public static function stream(sound: Sound, loop: Bool = false): kha.audio1.AudioChannel;
 }
