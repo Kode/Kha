a140dc15b0e960e913dc3bb1cd3c70b02b27bd62
David Klein <dave@smokinheads.de>
2017-01-09 12:22:20 +0100

    expose x/y/isPrimary display properties

--- Backends/Flash/kha/DisplayImpl.hx
+++ Backends/Flash/kha/DisplayImpl.hx
@@ -5,11 +5,23 @@ class DisplayImpl {
         return 1;
     }
 
-    public static function width( id : Int ) : Int {
+    public static function width(index: Int): Int {
         return flash.Lib.current.stage.fullScreenWidth;
     }
 
-    public static function height( id : Int ) : Int {
+    public static function height(index: Int): Int {
         return flash.Lib.current.stage.fullScreenHeight;
+    }
+
+    public static function x(index: Int): Int {
+        return flash.Lib.current.stage.x;
+    }
+
+    public static function y(index: Int): Int {
+        return flash.Lib.current.stage.y;
+    }
+
+    public static function isPrimary(index: Int): Bool {
+        return true; // TODO (DK) is there any way to figure out what display flash opened on?
     }
 }
