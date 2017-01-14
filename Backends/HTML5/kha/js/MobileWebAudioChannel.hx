a6320ba463ee6a335882aade0d96dacaaaef847c
Robert Konrad <git2016@robdangero.us>
2017-01-15 00:11:42 +0100

    Try to fix #485

--- Backends/HTML5/kha/js/MobileWebAudioChannel.hx
+++ Backends/HTML5/kha/js/MobileWebAudioChannel.hx
@@ -1,9 +1,12 @@
 package kha.js;
 
+import js.html.audio.AudioBuffer;
 import js.html.audio.AudioBufferSourceNode;
 import js.html.audio.GainNode;
 
 class MobileWebAudioChannel implements kha.audio1.AudioChannel {
+	private var buffer: AudioBuffer;
+	private var loop: Bool;
 	private var source: AudioBufferSourceNode;
 	private var gain: GainNode;
 	private var startTime: Float;
@@ -12,9 +15,15 @@ class MobileWebAudioChannel implements kha.audio1.AudioChannel {
 	private var stopped: Bool = false;
 
 	public function new(sound: MobileWebAudioSound, loop: Bool) {
+		this.buffer = sound._buffer;
+		this.loop = loop;
+		createSource();
+	}
+
+	private function createSource(): Void {
 		source = MobileWebAudio._context.createBufferSource();
 		source.loop = loop;
-		source.buffer = sound._buffer;
+		source.buffer = buffer;
 		source.onended = function () {
 			stopped = true;
 		}
@@ -24,8 +33,12 @@ class MobileWebAudioChannel implements kha.audio1.AudioChannel {
 	}
 	
 	public function play(): Void {
+		if (paused || stopped) {
+			createSource();
+		}
 		stopped = false;
 		if (paused) {
+			paused = false;
 			startTime = MobileWebAudio._context.currentTime - pauseTime;
 			source.start(0, pauseTime);
 		}
