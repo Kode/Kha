50e1e1da2cf39c57d5c036e41dfea46929607d2a
vujadin.krtolica <vujadin@gamestudiohx.com>
2017-01-27 19:39:57 +0100

    fix isOpaque() and at() methods

--- Backends/HTML5/kha/WebGLImage.hx
+++ Backends/HTML5/kha/WebGLImage.hx
@@ -35,12 +35,15 @@ class WebGLImage extends Image {
 	private var depthStencilFormat: DepthStencilFormat;
 
 	public static function init() {
-		var canvas: Dynamic = Browser.document.createElement("canvas");
-		if (canvas != null) {
-			context = canvas.getContext("2d");
-			canvas.width = 2048;
-			canvas.height = 2048;
-			context.globalCompositeOperation = "copy";
+		if (context == null) {
+			// create only once
+			var canvas: Dynamic = Browser.document.createElement("canvas");
+			if (canvas != null) {
+				context = canvas.getContext("2d");
+				canvas.width = 2048;
+				canvas.height = 2048;
+				context.globalCompositeOperation = "copy";
+			}
 		}
 	}
 
@@ -52,6 +55,7 @@ class WebGLImage extends Image {
 		image = null;
 		video = null;
 		this.depthStencilFormat = depthStencilFormat;
+		init();
 		if (renderTarget) createTexture();
 	}
 
