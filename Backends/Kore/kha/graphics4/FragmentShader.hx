84872c011e946b5843b263c304873436edde66f5
Robert Konrad <git2016@robdangero.us>
2017-04-10 13:53:03 +0200

    Implement Shader.fromSource for Kore
    Fixes #318

--- Backends/Kore/kha/graphics4/FragmentShader.hx
+++ Backends/Kore/kha/graphics4/FragmentShader.hx
@@ -17,11 +17,19 @@ import kha.Blob;
 @:headerClassCode("Kore::Graphics4::Shader* shader;")
 class FragmentShader {
 	public function new(sources: Array<Blob>, files: Array<String>) {
-		init(sources[0], files[0]);
+		if (sources != null) {
+			init(sources[0], files[0]);
+		}
 	}
 	
 	private function init(source: Blob, file: String): Void {
 		untyped __cpp__('shader = new Kore::Graphics4::Shader(source->bytes->b->Pointer(), source->get_length(), Kore::Graphics4::FragmentShader);');
+	}
+
+	public static function fromSource(source: String): FragmentShader {
+		var fragmentShader = new FragmentShader(null, null);
+		untyped __cpp__('fragmentShader->shader = new Kore::Graphics4::Shader(source, Kore::Graphics4::FragmentShader);');
+		return fragmentShader;
 	}
 	
 	public function delete(): Void {
--- Backends/Kore/kha/graphics4/VertexShader.hx
+++ Backends/Kore/kha/graphics4/VertexShader.hx
@@ -17,11 +17,19 @@ import kha.Blob;
 @:headerClassCode("Kore::Graphics4::Shader* shader;")
 class VertexShader {
 	public function new(sources: Array<Blob>, files: Array<String>) {
-		init(sources[0], files[0]);
+		if (sources != null) {
+			init(sources[0], files[0]);
+		}
 	}
 	
 	private function init(source: Blob, file: String): Void {
 		untyped __cpp__('shader = new Kore::Graphics4::Shader(source->bytes->b->Pointer(), source->get_length(), Kore::Graphics4::VertexShader);');
+	}
+
+	public static function fromSource(source: String): VertexShader {
+		var vertexShader = new VertexShader(null, null);
+		untyped __cpp__('vertexShader->shader = new Kore::Graphics4::Shader(source, Kore::Graphics4::VertexShader);');
+		return vertexShader;
 	}
 	
 	public function delete(): Void {
