b252ba20ba7424e57ee966b903e2a3ef7cdf967d
Lubos Lenco <lubos.lenco@gmail.com>
2017-06-30 15:59:39 +0200

    Update node backend

--- Backends/Node/kha/graphics4/FragmentShader.hx
+++ Backends/Node/kha/graphics4/FragmentShader.hx
@@ -6,4 +6,8 @@ class FragmentShader {
 	public function new(sources: Array<Blob>, files: Array<String>) {
 		
 	}
+
+	public static function fromSource(source: String): FragmentShader {
+		return null;
+	}
 }
