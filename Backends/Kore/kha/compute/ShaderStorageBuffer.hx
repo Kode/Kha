22a2e1532a1c94755ec6449d5d0d08363990ee0f
Lubos Lenco <lubos.lenco@gmail.com>
2017-08-03 10:47:05 +0200

    Fix ShaderStorageBuffer compile for Kore

--- Backends/Kore/kha/compute/ShaderStorageBuffer.hx
+++ Backends/Kore/kha/compute/ShaderStorageBuffer.hx
@@ -20,22 +20,22 @@ class ShaderStorageBuffer {
 	}
   
   @:functionCode("
-		Kore::VertexData type2;
+		Kore::Graphics4::VertexData type2;
     switch (type->index) {
     case 0:
-      type2 = Kore::Float1VertexData;
+      type2 = Kore::Graphics4::Float1VertexData;
       break;
     case 1:
-      type2 = Kore::Float2VertexData;
+      type2 = Kore::Graphics4::Float2VertexData;
       break;
     case 2:
-      type2 = Kore::Float3VertexData;
+      type2 = Kore::Graphics4::Float3VertexData;
       break;
     case 3:
-      type2 = Kore::Float4VertexData;
+      type2 = Kore::Graphics4::Float4VertexData;
       break;
     case 4:
-      type2 = Kore::Float4x4VertexData;
+      type2 = Kore::Graphics4::Float4x4VertexData;
       break;
     }
 		buffer = new Kore::ShaderStorageBuffer(indexCount, type2);
