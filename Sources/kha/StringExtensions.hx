de7fde4117824a2b4b1516e36bee1c970de2ef2c
Robert Konrad <git2016@robdangero.us>
2017-08-16 21:04:16 +0200

    Add StringExtensions class by @tienery
    Fixes #641

---
+++ Sources/kha/StringExtensions.hx
@@ -0,0 +1,11 @@
+package kha;
+
+class StringExtensions {
+	public static function toCharArray(s: String): Array<Int> {
+		var results = new Array<Int>();
+		for (i in 0...s.length) {
+            results.push(s.charCodeAt(i));
+		}
+		return results;
+	}
+}
