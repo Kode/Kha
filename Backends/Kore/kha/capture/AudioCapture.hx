73770eac1525ab80de7c255d0de51dc449b68c04
Lewis Lepton <lewisleptondev@gmail.com>
2017-12-09 13:06:33 +0000

    renamed Sources & Backends from Audio & Video capture to AudioCapture & VideoCapture

--- Backends/Kore/kha/capture/AudioCapture.hx
+++ Backends/Kore/kha/capture/AudioCapture.hx
@@ -2,7 +2,7 @@ package kha.capture;
 
 import kha.audio2.Buffer;
 
-class Audio {
+class AudioCapture {
 	public static var audioCallback: Int->Buffer->Void;
 	
 	public static function init(initialized: Void->Void, error: Void->Void): Void {
--- Sources/kha/capture/AudioCapture.hx
+++ Sources/kha/capture/AudioCapture.hx
@@ -2,7 +2,7 @@ package kha.capture;
 
 import kha.audio2.Buffer;
 
-extern class Audio {
+extern class AudioCapture {
 	public static var audioCallback: Int->Buffer->Void;
 	public static function init(initialized: Void->Void, error: Void->Void): Void;
 }
