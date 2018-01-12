c1dd84f9d6559d9731759d7b258513639bc29e3e
Robert Konrad <git2016@robdangero.us>
2018-01-12 14:53:26 +0100

    Print WebSocket close reasons

--- Backends/HTML5/kha/network/Network.hx
+++ Backends/HTML5/kha/network/Network.hx
@@ -11,7 +11,7 @@ class Network {
 	public function new(url: String, port: Int, errorCallback: Void->Void, closeCallback: Void->Void) {
 		socket = new WebSocket("ws://" + url + ":" + port);
 		socket.onerror = function (error) {
-			trace("Network error: " + error);
+			trace("Network error.");
 			errorCallback();
 		}
 		socket.binaryType = BinaryType.ARRAYBUFFER;
@@ -19,8 +19,26 @@ class Network {
 			open = true;
 		};
 		socket.onclose = function (event) {
-			trace("Network connection closed");
+			trace("Network connection closed. " + webSocketCloseReason(event.code) + " (" + event.reason + ").");
 			closeCallback();
+		}
+	}
+
+	static function webSocketCloseReason(code: Int): String {
+		switch (code) {
+			case 1000: return "Normal Closure";
+			case 1001: return "Going Away";
+			case 1002: return "Protocol error";
+			case 1003: return "Unsupported Data";
+			case 1005: return "No Status Rcvd";
+			case 1006: return "Abnormal Closure";
+			case 1007: return "Invalid frame";
+			case 1008: return "Policy Violation";
+			case 1009: return "Message Too Big";
+			case 1010: return "Mandatory Ext.";
+			case 1011: return "Internal Server Error";
+			case 1015: return "TLS handshake";
+			default: return "";
 		}
 	}
 	
