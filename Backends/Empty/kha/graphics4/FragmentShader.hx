52690fab6b64074be9892e37de0adbf5605514c1
Robert Konrad <git2016@robdangero.us>
2018-02-26 12:01:49 +0100

    Update Empty backend

--- Backends/Empty/kha/graphics4/FragmentShader.hx
+++ Backends/Empty/kha/graphics4/FragmentShader.hx
@@ -1,11 +1,21 @@
 package kha.graphics4;
 
 class FragmentShader {
-	public var source: String;
+	public var sources: Array<String>;
 	public var type: Dynamic;
 	public var shader: Dynamic;
+	public var files: Array<String>;
 	
-	public function new(source: Blob, file: String) {
+	public function new(sources: Array<Blob>, files: Array<String>) {
 		
+	}
+	
+	public static function fromSource(source: String): FragmentShader {
+		return null;
+	}
+	
+	public function delete(): Void {
+		shader = null;
+		sources = null;
 	}
 }
