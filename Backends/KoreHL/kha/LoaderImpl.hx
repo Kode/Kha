405fb5ca10ea66c88f01f302277733ff2a81f6b8
David Klein <dave@smokinheads.de>
2018-04-12 21:15:02 +0200

    add missing failure callbacks

--- Backends/KoreHL/kha/LoaderImpl.hx
+++ Backends/KoreHL/kha/LoaderImpl.hx
@@ -6,7 +6,7 @@ import kha.Kravur;
 import sys.io.File;
 
 class LoaderImpl {
-	public static function loadSoundFromDescription(desc: Dynamic, done: kha.Sound -> Void) {
+	public static function loadSoundFromDescription(desc: Dynamic, done: kha.Sound -> Void, failed: AssetError -> Void) {
 		done(new kha.korehl.Sound(desc.files[0]));
 	}
 	
@@ -14,7 +14,7 @@ class LoaderImpl {
 		return ["wav", "ogg"];
 	}
 	
-	public static function loadImageFromDescription(desc: Dynamic, done: kha.Image -> Void) {
+	public static function loadImageFromDescription(desc: Dynamic, done: kha.Image -> Void, failed: AssetError -> Void) {
 		var readable = Reflect.hasField(desc, "readable") ? desc.readable : false;
 		done(kha.Image.fromFile(desc.files[0], readable));
 	}
@@ -23,17 +23,17 @@ class LoaderImpl {
 		return ["png", "jpg", "hdr"];
 	}
 	
-	public static function loadBlobFromDescription(desc: Dynamic, done: Blob -> Void) {
+	public static function loadBlobFromDescription(desc: Dynamic, done: Blob -> Void, failed: AssetError -> Void) {
 		done(new Blob(File.getBytes(desc.files[0])));
 	}
 	
-	public static function loadFontFromDescription(desc: Dynamic, done: Font -> Void): Void {
+	public static function loadFontFromDescription(desc: Dynamic, done: Font -> Void, failed: AssetError -> Void): Void {
 		loadBlobFromDescription(desc, function (blob: Blob) {
 			done(new Kravur(blob));
-		});
+		}, failed);
 	}
 	
-	public static function loadVideoFromDescription(desc: Dynamic, done: Video -> Void) {
+	public static function loadVideoFromDescription(desc: Dynamic, done: Video -> Void, failed: AssetError -> Void) {
 		done(null); //new kha.kore.Video(desc.files[0]));
 	}
 	
