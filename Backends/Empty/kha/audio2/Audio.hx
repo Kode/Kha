972aed30732106b855b609747b8ab81d671ccf88
Robert Konrad <git2016@robdangero.us>
2018-05-12 13:40:55 +0200

    Remove bogus Audio2.play method in the Empty backend

--- Backends/Empty/kha/audio2/Audio.hx
+++ Backends/Empty/kha/audio2/Audio.hx
@@ -5,10 +5,6 @@ import kha.Sound;
 class Audio {
 	public static var audioCallback: Int->Buffer->Void;
 	
-	public static function play(sound: Sound, loop: Bool = false): kha.audio1.AudioChannel {
-		return null;
-	}
-	
 	public static function stream(sound: Sound, loop: Bool = false): kha.audio1.AudioChannel {
 		return null;
 	}
