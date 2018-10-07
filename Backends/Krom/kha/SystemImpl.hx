6384b38bdc9286bb4e06686f99f3ba2d26fa5005
luboslenco <lubos.lenco@gmail.com>
2018-10-07 23:13:27 +0200

    Cut copy paste for Krom

--- Backends/Krom/kha/SystemImpl.hx
+++ Backends/Krom/kha/SystemImpl.hx
@@ -29,6 +29,30 @@ class SystemImpl {
 	private static function dropFilesCallback(filePath: String): Void {
 		System.dropFiles(filePath);
 	}
+
+	private static function copyCallback(): String {
+		if (System.copyListener != null) {
+			return System.copyListener();
+		}
+		else {
+			return null;
+		}
+	}
+	
+	private static function cutCallback(): String {
+		if (System.cutListener != null) {
+			return System.cutListener();
+		}
+		else {
+			return null;
+		}
+	}
+	
+	private static function pasteCallback(data: String): Void {
+		if (System.pasteListener != null) {
+			System.pasteListener(data);
+		}
+	}
 		
 	private static function keyboardDownCallback(code: Int): Void {
 		keyboard.sendDownEvent(cast code);
@@ -120,6 +144,7 @@ class SystemImpl {
 		framebuffer.init(new kha.graphics2.Graphics1(framebuffer), new kha.graphics4.Graphics2(framebuffer), g4);
 		Krom.setCallback(renderCallback);
 		Krom.setDropFilesCallback(dropFilesCallback);
+		Krom.setCutCopyPasteCallback(cutCallback, copyCallback, pasteCallback);
 		
 		keyboard = new Keyboard();
 		mouse = new MouseImpl();
