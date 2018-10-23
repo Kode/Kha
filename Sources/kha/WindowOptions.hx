55153c322765c50b167e5891a206a70ce752ffae
Jarrio <Jarrio@users.noreply.github.com>
2018-10-23 18:02:37 +0100

    Made WindowFeatures an abstract enum

--- Sources/kha/WindowOptions.hx
+++ Sources/kha/WindowOptions.hx
@@ -1,13 +1,14 @@
 package kha;
+@:enum abstract WindowFeatures(Int) from Int to Int {
+	var FeatureResizable = 1;
+	var FeatureMinimizable = 2;
+	var FeatureMaximizable = 4;
+	var FeatureBorderless = 8;
+	var FeatureOnTop = 16;
+}
 
 @:structInit
 class WindowOptions {
-	public static inline var FeatureResizable = 1;
-	public static inline var FeatureMinimizable = 2;
-	public static inline var FeatureMaximizable = 4;
-	public static inline var FeatureBorderless = 8;
-	public static inline var FeatureOnTop = 16;
-
 	@:optional public var title: String = null;
 	@:optional public var x: Int = -1;
 	@:optional public var y: Int = -1;
@@ -15,11 +16,11 @@ class WindowOptions {
 	@:optional public var height: Int = 600;
 	@:optional public var display: Display = null;
 	@:optional public var visible: Bool = true;
-	@:optional public var windowFeatures: Int = FeatureResizable | FeatureMaximizable | FeatureMinimizable;
+	@:optional public var windowFeatures:WindowFeatures = FeatureResizable | FeatureMaximizable | FeatureMinimizable;
 	@:optional public var mode: WindowMode = Windowed;
 
 	public function new(title: String = null, ?x: Int = -1, ?y: Int = -1, ?width: Int = 800, ?height: Int = 600, display: Display = null,
-	?visible: Bool = true, ?windowFeatures: Int = FeatureResizable | FeatureMaximizable | FeatureMinimizable, ?mode: WindowMode = WindowMode.Windowed) {
+	?visible: Bool = true, ?windowFeatures:WindowFeatures = FeatureResizable | FeatureMaximizable | FeatureMinimizable, ?mode: WindowMode = WindowMode.Windowed) {
 		this.title = title;
 		this.x = x;
 		this.y = y;
