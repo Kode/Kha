dd0eabb7fb41a4b52dd418b63c0139abdb205124
RblSb <msrblsb@gmail.com>
2019-04-26 08:06:15 +0300

    Optimize android-java

--- Backends/Android/kha/arrays/Float32Array.hx
+++ Backends/Android/kha/arrays/Float32Array.hx
@@ -6,30 +6,30 @@ import java.nio.FloatBuffer;
 
 abstract Float32Array(FloatBuffer) {
 	private static inline var elementSize = 4;
-		
+
 	public inline function new(elements: Int) {
-		this = ByteBuffer.allocateDirect(elements * elementSize).order(ByteOrder.nativeOrder()).asFloatBuffer();
+		this = FloatBuffer.allocate(elements * elementSize);
 	}
-	
+
 	public var length(get, never): Int;
 
 	inline function get_length(): Int {
 		return this.remaining();
 	}
-	
-	public function set(index: Int, value: FastFloat): FastFloat {
+
+	public inline function set(index: Int, value: FastFloat): FastFloat {
 		this.put(index, value);
 		return value;
 	}
-	
+
 	public inline function get(index: Int): FastFloat {
 		return this.get(index);
 	}
-	
+
 	public inline function data(): FloatBuffer {
 		return this;
 	}
-	
+
 	@:arrayAccess
 	public inline function arrayRead(index: Int): FastFloat {
 		return this.get(index);
