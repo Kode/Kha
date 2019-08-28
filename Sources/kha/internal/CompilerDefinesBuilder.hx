9442df7b047060fcd955f737d28fbefc9baa99ac
RblSb <msrblsb@gmail.com>
2019-08-28 17:04:34 +0300

    Check for invalid defines

--- Sources/kha/internal/CompilerDefinesBuilder.hx
+++ Sources/kha/internal/CompilerDefinesBuilder.hx
@@ -15,18 +15,47 @@ class CompilerDefinesBuilder {
 	 *
 	 * @return	Array of class fields.
 	 */
-	public static macro function build() : Array<Field> {
-		var fields = Context.getBuildFields();
-
-		var defines = Context.getDefines();
+	public static macro function build(): Array<Field> {
+		final fields = Context.getBuildFields();
+		final defines = Context.getDefines();
+		final added:Map<String, Bool> = [];
 
 		for (k in defines.keys()) {
-			var key = $v{k};
-			if (key == "true" || key == "static" || key.indexOf("-") != -1) continue;
+			final key = prepareFieldName($v{k});
+			if (added[key]) continue;
+			added[key] = true;
 			addField(fields, key, $v{Std.string(defines.get(key))});
 		}
 
 		return fields;
+	}
+
+	/**
+	 * Fix wrong field names like keywords.
+	 *
+	 * @param	name	Field name.
+	 * @return	New field name.
+	 */
+	static function prepareFieldName(name: String): String {
+		if (isKeyword(name)) return '_$name';
+		name = ~/[^A-z0-9_]/.replace(name, "_");
+		if (~/^[0-9]/.match(name)) return '_$name';
+		return name;
+	}
+
+	static final keywords = [
+		"function", "class", "static", "var", "if", "else", "while", "do", "for",
+		"break", "return", "continue", "extends", "implements", "import",
+		"switch", "case", "default", "public", "private", "try", "untyped",
+		"catch", "new", "this", "throw", "extern", "enum", "in", "interface",
+		"cast", "override", "dynamic", "typedef", "package",
+		"inline", "using", "null", "true", "false", "abstract", "macro", "final",
+		"operator", "overload"
+	];
+
+	/** Checks for keywords (see `is_valid_identifier` from Haxe) */
+	static function isKeyword(name: String): Bool {
+		return keywords.indexOf(name) != -1;
 	}
 
 	/**
@@ -37,7 +66,7 @@ class CompilerDefinesBuilder {
 	 * @param	name	Field name.
 	 * @param	value	Field value.
 	 */
-	static function addField(fields : Array<Field>, name : String, value : String) : Void {
+	static function addField(fields: Array<Field>, name: String, value: String): Void {
 		fields.push({
 			name: name,
 			access: [ APublic, AStatic ],
