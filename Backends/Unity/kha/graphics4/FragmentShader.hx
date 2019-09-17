8c6f945cf277efdbc6db864336f34386b4c7b293
Will Thompson <william.thompsonj@gmail.com>
2019-09-17 19:16:07 +0200

    Unity Update
    Normalized white space to use \n for all line endings, removed unnecessary whitespace.
    
    Added empty functions for required functions defined by extern classes.
    
    Created the following files:
    - audio\Audio.hx
    - Display.hx
    - netsync\Network.hx
    - Window.hx

--- Backends/Unity/kha/graphics4/FragmentShader.hx
+++ Backends/Unity/kha/graphics4/FragmentShader.hx
@@ -3,9 +3,13 @@ package kha.graphics4;
 import kha.Blob;
 
 class FragmentShader {
+	public var shader: Dynamic;
 	public var name: String;
 
-	public function new(source: Blob, file: String) {
-		name = source.toString();
+	public function new(sources: Array<Blob>, names: Array<String>) {
+		if (sources != null) {
+			shader = sources[0].bytes.getData();
+			name = names[0];
+		}
 	}
 }
