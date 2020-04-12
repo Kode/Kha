68c2b29ba4e575e27300137874d73308a1ee16a1
Robert Konrad <git2016@robdangero.us>
2020-04-12 15:58:05 +0200

    Add a comment for FastFloat

--- Sources/kha/FastFloat.hx
+++ Sources/kha/FastFloat.hx
@@ -1,5 +1,15 @@
 package kha;
 
+/*
+	FastFloat uses 32 bit floats wherever that is possible.
+	But JavaScript in particular only supports 64 bit floats.
+	Therefore when using FastFloat you will have different
+	precision on different targets and therefore it is
+	strongly advised to only use it where that does not
+	matter (typically graphics code,  avoid it in gameplay
+	code at all costs).
+*/
+
 #if cpp
 typedef FastFloat = cpp.Float32;
 #elseif hl
