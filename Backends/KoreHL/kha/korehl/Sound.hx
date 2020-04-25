be895e13c993037474038faf72343e782c2c618a
Valentín Barros <valentin@sanva.net>
2020-04-25 16:31:56 +0200

    Fixes playing `.ogg` sound in Android.

--- Backends/KoreHL/kha/korehl/Sound.hx
+++ Backends/KoreHL/kha/korehl/Sound.hx
@@ -8,25 +8,18 @@ using StringTools;
 @:keep
 class Sound extends kha.Sound {
 
-	function initWav(filename: String) {
+	function initSound(filename: String) {
 		uncompressedData = new kha.arrays.Float32Array();
 		var dataSize = new kha.arrays.Uint32Array(1);
 		var data = kore_sound_init_wav(StringHelper.convert(filename), dataSize.getData(), length);
 		uncompressedData.setData(data, dataSize[0]);
 		dataSize.free();
 	}
-
-	function initOgg(filename: String) {
-		compressedData = File.getBytes(filename);
-	}
 	
 	public function new(filename: String) {
 		super();
-		if (filename.endsWith(".wav")) {
-			initWav(filename);
-		}
-		else if (filename.endsWith(".ogg")) {
-			initOgg(filename);
+		if (filename.endsWith(".wav") || filename.endsWith(".ogg")) {
+			initSound(filename);
 		}
 		else {
 			trace("Unknown sound format: " + filename);
