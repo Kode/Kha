c07c9c90c76c86dc753e62aa48e8105ee7aa06cf
Moritz Brückner <mail@moritz-brueckner.de>
2020-05-12 22:58:20 +0200

    Implement delete() for shaders on nodejs target

--- Backends/Node/kha/graphics4/VertexShader.hx
+++ Backends/Node/kha/graphics4/VertexShader.hx
@@ -4,10 +4,14 @@ import kha.Blob;
 
 class VertexShader {
 	public function new(sources: Array<Blob>, files: Array<String>) {
-		
+
 	}
 
 	public static function fromSource(source: String): VertexShader {
 		return null;
+	}
+
+	public function delete(): Void {
+
 	}
 }
