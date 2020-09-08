21afa9f21fe6c46e5ad254afb108142e5b9cc677
David Klein <david.klein@psmtec.de>
2020-09-08 09:34:52 +0200

    fix compilation on node target

---
+++ Backends/Node/kha/Display.hx
@@ -0,0 +1,10 @@
+package kha;
+
+class Display {
+	public static final primary = new Display();
+	public static var all = [];
+	public final frequency = 60;
+	public final pixelsPerInch = 96;
+
+	function new() {}
+}
