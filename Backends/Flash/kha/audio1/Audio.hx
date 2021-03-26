79af1ed961f2d64bfb61f75487801381f5af5f67
Robert Konrad <git2016@robdangero.us>
2021-03-26 14:10:31 +0100

    Delete Flash \o/

--- Backends/Flash/kha/audio1/Audio.hx
+++
@@ -1,3 +0,0 @@
-package kha.audio1;
-
-typedef Audio = kha.audio2.Audio1;
--- Backends/Flash/kha/flash/graphics4/TextureUnit.hx
+++
@@ -1,7 +0,0 @@
-package kha.flash.graphics4;
-
-class TextureUnit implements kha.graphics4.TextureUnit {
-	public var unit: Int;
-
-	public function new() {}
-}
