243b0c7e6b3423167807880e23a76fa3e92d239a
Robert Konrad <git2016@robdangero.us>
2022-02-27 18:24:58 +0100

    Make sure no threading-weirdness can happen with AudioChannel-properties

--- Sources/kha/audio2/AudioChannel.hx
+++ Sources/kha/audio2/AudioChannel.hx
@@ -2,14 +2,73 @@ package kha.audio2;
 
 import kha.arrays.Float32Array;
 
+@:headerCode("#include <kinc/threads/atomic.h>")
+@:headerClassCode("volatile float kinc_volume; volatile int kinc_position; volatile int kinc_paused; volatile int kinc_stopped; volatile int kinc_looping;")
 class AudioChannel implements kha.audio1.AudioChannel {
 	public var data: Float32Array = null;
 
+#if cpp
+	var myVolume(get, set): Float;
+
+	inline function get_myVolume(): Float {
+		return untyped __cpp__("kinc_volume");
+	}
+
+	inline function set_myVolume(value: Float): Float {
+		untyped __cpp__("KINC_ATOMIC_EXCHANGE_FLOAT(&kinc_volume, (float){0})", value);
+		return value;
+	}
+
+	var myPosition(get, set): Int;
+
+	inline function get_myPosition(): Int {
+		return untyped __cpp__("kinc_position");
+	}
+
+	inline function set_myPosition(value: Int): Int {
+		untyped __cpp__("KINC_ATOMIC_EXCHANGE_32(&kinc_position, {0})", value);
+		return value;
+	}
+
+	var paused(get, set): Bool;
+
+	inline function get_paused(): Bool {
+		return untyped __cpp__("kinc_paused != 0");
+	}
+
+	inline function set_paused(value: Bool): Bool {
+		untyped __cpp__("KINC_ATOMIC_EXCHANGE_32(&kinc_paused, {0} ? 1 : 0)", value);
+		return value;
+	}
+
+	var stopped(get, set): Bool;
+
+	inline function get_stopped(): Bool {
+		return untyped __cpp__("kinc_stopped != 0");
+	}
+
+	inline function set_stopped(value: Bool): Bool {
+		untyped __cpp__("KINC_ATOMIC_EXCHANGE_32(&kinc_stopped, {0} ? 1 : 0)", value);
+		return value;
+	}
+
+	var looping(get, set): Bool;
+
+	inline function get_looping(): Bool {
+		return untyped __cpp__("kinc_looping != 0");
+	}
+
+	inline function set_looping(value: Bool): Bool {
+		untyped __cpp__("KINC_ATOMIC_EXCHANGE_32(&kinc_looping, {0} ? 1 : 0)", value);
+		return value;
+	}
+#else
 	var myVolume: Float = 1;
 	var myPosition: Int = 0;
 	var paused: Bool = false;
 	var stopped: Bool = false;
 	var looping: Bool = false;
+#end
 
 	public function new(looping: Bool) {
 		this.looping = looping;
