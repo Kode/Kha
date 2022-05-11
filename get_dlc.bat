009c1468196edae60792b73a0e63bf0eae0e90aa
Robert Konrad <git2016@robdangero.us>
2022-05-11 23:15:35 +0200

    Improve the dlc-bat

--- get_dlc.bat
+++ get_dlc.bat
@@ -2,4 +2,4 @@
 @git -C %~dp0 submodule update --init Backends/Kinc-hxcpp/khacpp
 @git -C %~dp0 submodule update --init Tools/windows_x64
 @git -C %~dp0 submodule update --init Kinc
-@call Kinc\get_dlc.bat
+@call %~dp0\Kinc\get_dlc.bat
--- get_dlc_dangerously.bat
+++ get_dlc_dangerously.bat
@@ -2,4 +2,4 @@
 @git -C %~dp0 submodule update --init --remote Backends/Kinc-hxcpp/khacpp
 @git -C %~dp0 submodule update --init --remote Tools/windows_x64
 @git -C %~dp0 submodule update --init --remote Kinc
-@call Kinc\get_dlc_dangerously.bat
+@call %~dp0\Kinc\get_dlc_dangerously.bat
