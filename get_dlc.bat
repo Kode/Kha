1e4ad5324b8b3cb77b733d584b6793f5d4aa443d
Robert Konrad <git2016@robdangero.us>
2022-05-11 22:30:23 +0200

    Add dlc scripts

---
+++ get_dlc.bat
@@ -0,0 +1,5 @@
+@git -C %~dp0 submodule update --init Tools/khamake
+@git -C %~dp0 submodule update --init Backends/Kinc-hxcpp/khacpp
+@git -C %~dp0 submodule update --init Tools/windows_x64
+@git -C %~dp0 submodule update --init Kinc
+@call Kinc\get_dlc.bat
