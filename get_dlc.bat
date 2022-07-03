1165e5ba9b17f75e67afecb2046fd2f571a919a5
Jurien Meerlo <codescapade@gmail.com>
2022-07-03 14:21:32 +0100

    Allow spaces in path on windows for get_dlc

--- get_dlc.bat
+++ get_dlc.bat
@@ -1,5 +1,7 @@
-@git -C %~dp0 submodule update --init Tools/khamake
-@git -C %~dp0 submodule update --init Backends/Kinc-hxcpp/khacpp
-@git -C %~dp0 submodule update --init Tools/windows_x64
-@git -C %~dp0 submodule update --init Kinc
-@call %~dp0\Kinc\get_dlc.bat
+@pushd "%~dp0"
+@git submodule update --init Tools/khamake
+@git submodule update --init Backends/Kinc-hxcpp/khacpp
+@git submodule update --init Tools/windows_x64
+@git submodule update --init Kinc
+@call Kinc\get_dlc.bat
+@popd
