5ee38c9a9defb9b981ee3742864b6f2fd06f030d
Robert Konrad <git2016@robdangero.us>
2023-02-11 03:45:29 +0100

    Document the intricacies of audio1.Audio.play

--- Sources/kha/audio1/Audio.hx
+++ Sources/kha/audio1/Audio.hx
@@ -3,6 +3,14 @@ package kha.audio1;
 import kha.Sound;
 
 extern class Audio {
+	/**
+	 * Plays a sound immediately.
+	 * @param sound
+	 * The sound to play
+	 * @param loop
+	 * Whether or not to automatically loop the sound
+	 * @return A channel object that can be used to control the playing sound. Please be a ware that Null is returned when the maximum number of simultaneously played channels was reached.
+	 */
 	public static function play(sound: Sound, loop: Bool = false): AudioChannel;
 
 	public static function stream(sound: Sound, loop: Bool = false): kha.audio1.AudioChannel;
