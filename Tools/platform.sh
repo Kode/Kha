cb8d9c424c9c0323c748d2c20cfbf48ac48939d8
Robert Konrad <git2016@robdangero.us>
2023-04-09 04:37:19 +0200

    Update some submodules

--- Tools/platform.sh
+++ Tools/platform.sh
@@ -1,8 +1,6 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	MACHINE_TYPE=`uname -m`
-	if [[ "$MACHINE_TYPE" == "armv"* ]]; then
-		KINC_PLATFORM=linux_arm
-	elif [[ "$MACHINE_TYPE" == "aarch64"* ]]; then
+	if [[ "$MACHINE_TYPE" == "aarch64"* ]]; then
 		KINC_PLATFORM=linux_arm64
 	elif [[ "$MACHINE_TYPE" == "x86_64"* ]]; then
 		KINC_PLATFORM=linux_x64
@@ -12,8 +10,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	KINC_PLATFORM=macos
-elif [[ "$OSTYPE" == "FreeBSD"* ]]; then
-	KINC_PLATFORM=freebsd_x64
 elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* ]]; then
 	KINC_PLATFORM=windows_x64
 	KINC_EXE_SUFFIX=.exe
