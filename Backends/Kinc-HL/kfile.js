fc113d2cf2f26217f4bbf550915f33ad6c9bb08f
Robert Konrad <git2016@robdangero.us>
2023-10-08 14:41:33 +0200

    Correct the hl kfile for fmt

--- Backends/Kinc-HL/kfile.js
+++ Backends/Kinc-HL/kfile.js
@@ -3,7 +3,7 @@ let project = new Project('Kha', __dirname);
 project.addFiles('kinc-bridge/**', 'hl/include/**', 'hl/src/std/**', 'hl/src/gc.c', 'hl/src/hl.h', 'hl/src/hlc.h', 'hl/src/hlmodule.h', 'hl/src/opcodes.h');
 project.addExcludes('hl/src/std/unicase.c', 'hl/src/std/debug.c', 'hl/include/pcre/pcre2_jit_match.c', 'hl/include/pcre/pcre2_jit_misc.c');
 project.addIncludeDirs('hl/src', 'hl/include/pcre', 'hl/include/mbedtls/include', 'hl/include/zlib');
-project.addFiles('hl/include/mbedtls/library/**', 'hl/include/zlib/**', 'hl/libs/fmt/fmt_crypto.c', 'hl/libs/fmt/sha1.c', 'hl/libs/ssl/ssl.c');
+project.addFiles('hl/include/mbedtls/library/**', 'hl/include/zlib/**', 'hl/libs/fmt/fmt.c', 'hl/libs/fmt/sha1.c', 'hl/libs/ssl/ssl.c');
 
 if (platform == Platform.OSX) project.addDefine('KORE_DEBUGDIR="osx-hl"');
 if (platform == Platform.iOS) project.addDefine('KORE_DEBUGDIR="ios-hl"');
