42b78552158b85f929d4bde2dbf9a965b69a626c
Robert Konrad <git2016@robdangero.us>
2023-11-23 10:50:27 +0100

    Work around Git sometimes ignoring the shallow-option

--- get_dlc.bat
+++ get_dlc.bat
@@ -1,7 +1,7 @@
 @pushd "%~dp0"
 @git submodule update --init Tools/khamake
 @git submodule update --init Backends/Kinc-hxcpp/khacpp
-@git submodule update --init Tools/windows_x64
+@git submodule update --depth 1 --init Tools/windows_x64
 @git submodule update --init Kinc
 @call Kinc\get_dlc.bat
 @popd
