dc306e8d24356b3c42d1e957c3ebdfee11b2e98e
luboslenco <lubos.lenco@gmail.com>
2023-12-04 14:19:15 +0100

    Fixes for armory

--- Backends/HTML5/kha/js/graphics4/Graphics.hx
+++ Backends/HTML5/kha/js/graphics4/Graphics.hx
@@ -622,8 +622,8 @@ class Graphics implements kha.graphics4.Graphics {
 		SystemImpl.gl.uniform4f((cast location : ConstantLocation).value, value.x, value.y, value.z, value.w);
 	}
 
+	static var matrixCache = new js.lib.Float32Array(16);
 	public inline function setMatrix(location: kha.graphics4.ConstantLocation, matrix: FastMatrix4): Void {
-		static var matrixCache = new js.lib.Float32Array(16);
 		matrixCache[0] = matrix._00;
 		matrixCache[1] = matrix._01;
 		matrixCache[2] = matrix._02;
@@ -643,8 +643,8 @@ class Graphics implements kha.graphics4.Graphics {
 		SystemImpl.gl.uniformMatrix4fv((cast location : ConstantLocation).value, false, matrixCache);
 	}
 
+	static var matrix3Cache = new js.lib.Float32Array(9);
 	public inline function setMatrix3(location: kha.graphics4.ConstantLocation, matrix: FastMatrix3): Void {
-		static var matrix3Cache = new js.lib.Float32Array(9);
 		matrix3Cache[0] = matrix._00;
 		matrix3Cache[1] = matrix._01;
 		matrix3Cache[2] = matrix._02;
