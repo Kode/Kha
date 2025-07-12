3e5838abb11711a528e319f029aab4ed7c927df9
Robert Konrad <git2016@robdangero.us>
2025-07-12 19:25:32 +0200

    Update the dlc scripts

--- Tools/platform.sh
+++ Tools/platform.sh
@@ -1,18 +1,26 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	MACHINE_TYPE=`uname -m`
 	if [[ "$MACHINE_TYPE" == "aarch64"* ]]; then
-		KINC_PLATFORM=linux_arm64
+		KORE_PLATFORM=linux_arm64
 	elif [[ "$MACHINE_TYPE" == "x86_64"* ]]; then
-		KINC_PLATFORM=linux_x64
+		KORE_PLATFORM=linux_x64
 	else
 		echo "Unknown Linux machine '$MACHINE_TYPE', please edit Tools/platform.sh"
 		exit 1
 	fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	KINC_PLATFORM=macos
+	MACHINE_TYPE=`uname -m`
+	if [[ "$MACHINE_TYPE" == "arm64"* ]]; then
+		KORE_PLATFORM=macos_arm64
+	elif [[ "$MACHINE_TYPE" == "x86_64"* ]]; then
+		KORE_PLATFORM=macos_x64
+	else
+		echo "Unknown macOS machine '$MACHINE_TYPE', please edit Tools/platform.sh"
+		exit 1
+	fi
 elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* ]]; then
-	KINC_PLATFORM=windows_x64
-	KINC_EXE_SUFFIX=.exe
+	KORE_PLATFORM=windows_x64
+	KORE_EXE_SUFFIX=.exe
 else
 	echo "Unknown platform '$OSTYPE', please edit Tools/platform.sh"
 	exit 1
